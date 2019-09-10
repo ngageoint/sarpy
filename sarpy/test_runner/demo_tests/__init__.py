c86b502d10ebb282fdb6f26ef39a1352d745f77d
Casey <caseyjt@east.ile.nga.mil>
2019-09-10 16:27:52 -0400

    added geographiclib data to unit test

--- sarpy/test_runner/demo_tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from sarpy import sarpy_support_dir
-from sarpy.utils import file_utils
-
-geoid_2008_1_fname = file_utils.get_path_from_subdirs(sarpy_support_dir, ['geoid_models',
-                                                                          'egm2008-1',
-                                                                          'geoids',
-                                                                          'egm2008-1.pgm'])
--- sarpy/test_runner/demo_tests/test_geoid_vs_ellipsoid.py
+++ sarpy/test_runner/demo_tests/test_geoid_vs_ellipsoid.py
@@ -1,9 +1,19 @@
 from __future__ import division
 
 from sarpy.io.DEM.geoid import GeoidHeight
-from sarpy.test_runner.demo_tests import geoid_2008_1_fname
+from sarpy import sarpy_support_dir
 import numpy as np
+from sarpy.utils import file_utils
 import unittest
+
+geoid_2008_1_fname = file_utils.get_path_from_subdirs(sarpy_support_dir, ['geoid_models',
+                                                                          'egm2008-1',
+                                                                          'geoids',
+                                                                          'egm2008-1.pgm'])
+
+geoid_test_data_fname = file_utils.get_path_from_subdirs(sarpy_support_dir, ['geoid_models',
+                                                                             'geoid_test_data',
+                                                                             'numpy_geoid_heights.npy'])
 
 
 class TestGeoidVsEllipsoid(unittest.TestCase):
@@ -31,8 +41,23 @@ class TestGeoidVsEllipsoid(unittest.TestCase):
               "and the computed results match to within a tolerance of "
               + str(tolerance) + " meters")
 
+    def test_egm2008_1_w_geographiclib_test_data(self):
+        geoid_height = GeoidHeight(geoid_2008_1_fname)
+        test_heights = np.load(geoid_test_data_fname)
+        dims = test_heights.shape
+        height_diffs = np.zeros(dims[0])
+        tolerance = 0.01
+        # decimate by a factor of 500 for speed
+        for i in np.arange(0, dims[0], 500):
+            test_line = test_heights[i]
+            lat = test_line[0]
+            lon = test_line[1]
+            egm2008_height = test_line[4]
+            height_diffs[i] = geoid_height.get(lat, lon) - egm2008_height
+        assert np.abs(height_diffs.min()) < tolerance
+        assert np.abs(height_diffs.max()) < tolerance
+        print("computed geoid heights match test data provided by geographiclib within " + str(tolerance) + " meters.")
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
