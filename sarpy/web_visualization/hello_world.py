d15a4720286c0d7e3d9cf0d845d85de1222dd7a1
jcasey-beamio <j.casey@beamio.net>
2019-09-16 16:13:03 -0400

    working flask example animating something to the canvas

--- sarpy/web_visualization/hello_world.py
+++ sarpy/web_visualization/hello_world.py
@@ -14,35 +14,56 @@ def index():
 <!DOCTYPE html>
 <html>
 <body>
-<canvas></canvas>
+<canvas id="canvas" width="300" height="300" style="border:1px solid #000000;">
+</canvas>
 <script>
-var canvas = document.getElementsByTagName('canvas');
-var ctx = canvas[0].getContext('2d');
+var sun = new Image();
+var moon = new Image();
+var earth = new Image();
+function init() {
+  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
+  window.requestAnimationFrame(draw);
+}
 
-var img = new Image();
-img.src = "/api/b";
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
 
-// it can't draw it at once. it has to wait till it is loaded
-//ctx.drawImage(img, 0, 0);
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0, 0, 300, 300); // clear canvas
 
-img.onload = function() {
-  img.style.display = 'none'; // I don't know why they hide it
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+  ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
+  ctx.save();
+  ctx.translate(150, 150);
 
-  console.log('WxH: ' + img.width + 'x' + img.height)
+  // Earth
+  var time = new Date();
+  ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+  ctx.translate(105, 0);
+  ctx.fillRect(0, -12, 40, 24); // Shadow
+  ctx.drawImage(earth, -12, -12);
 
-  // convert Image to ImageData
-  //(it has to draw on canvas so it could need second canvas for this)
+  // Moon
+  ctx.save();
+  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+  ctx.translate(0, 28.5);
+  ctx.drawImage(moon, -3.5, -3.5);
+  ctx.restore();
 
-  ctx.drawImage(img, 0, 0);
-  var imageData = ctx.getImageData(0, 0, img.width, img.height)
+  ctx.restore();
+  
+  ctx.beginPath();
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+  ctx.stroke();
+ 
+  ctx.drawImage(sun, 0, 0, 300, 300);
 
-  //put ImageData many times  
-  for(x = 0 ; x < 100 ; x += 10) {
-    for(y = 0 ; y < 50 ; y += 10) {
-       ctx.putImageData(imageData, x, y);
-    }
-  }
-};
+  window.requestAnimationFrame(draw);
+}
+
+init();
 </script>
 </body>
 </html>
