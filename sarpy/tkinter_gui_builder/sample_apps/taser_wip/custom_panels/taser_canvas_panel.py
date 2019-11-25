e1d7a384a139210b3e957eda020531e6f66e5164
Jason Casey <j.casey@beamio.net>
2019-11-25 13:07:22 -0500

    added taser canvas panel as a custom widget.

---
+++ sarpy/tkinter_gui_builder/sample_apps/taser_wip/custom_panels/taser_canvas_panel.py
@@ -0,0 +1,6 @@
+from sarpy.tkinter_gui_builder.panel_templates.basic_image_canvas_panel import BasicImageCanvasPanel
+
+
+class TaserImageCanvasPanel(BasicImageCanvasPanel):
+    def __init__(self, master):
+        BasicImageCanvasPanel.__init__(self, master)
--- sarpy/tkinter_gui_builder/sample_apps/taser_wip/taser_wip.py
+++ sarpy/tkinter_gui_builder/sample_apps/taser_wip/taser_wip.py
@@ -2,7 +2,7 @@ import tkinter
 from tkinter.filedialog import askopenfilename
 from sarpy.tkinter_gui_builder.sample_apps.taser_wip.custom_panels.button_panel import ButtonPanel
 from sarpy.tkinter_gui_builder.panel_templates.basic_pyplot_image_panel import BasicPyplotImagePanel
-from sarpy.tkinter_gui_builder.panel_templates.basic_image_canvas_panel import BasicImageCanvasPanel
+from sarpy.tkinter_gui_builder.sample_apps.taser_wip.custom_panels.taser_canvas_panel import TaserImageCanvasPanel
 import numpy as np
 import imageio
 import os
@@ -24,7 +24,7 @@ class Taser:
         self.button_panel = ButtonPanel(master_frame)
         self.button_panel.set_spacing_between_buttons(0)
         self.pyplot_panel = BasicPyplotImagePanel(master_frame, 800, 600)
-        self.basic_image_panel = BasicImageCanvasPanel(master_frame)
+        self.basic_image_panel = TaserImageCanvasPanel(master_frame)
         self.basic_image_panel.set_canvas_size(600, 400)
 
         # specify layout of widget_wrappers in master frame
