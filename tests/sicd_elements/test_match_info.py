bdec27b35ac01fc12cb9b902b3f454f4cdb86b02
Thomas McCullough <thomas.mccullough@nga.mil>
2019-12-10 16:15:22 -0500

    Adding unit tests for MatchInfo

---
+++ tests/sicd_elements/test_match_info.py
@@ -0,0 +1,84 @@
+import sys
+from xml.etree import ElementTree
+
+if sys.version_info[0] < 3:
+    # so we can use subtests, which is pretty handy
+    import unittest2 as unittest
+else:
+    import unittest
+
+from sarpy.sicd_elements import MatchInfo
+
+match_collection_dict = {'CoreName': 'Name', 'MatchIndex': 2, 'Parameters': [{'name': 'PName', 'value': 'PValue'}, ]}
+match_dict = {
+    'TypeId': 'COHERENT', 'CurrentIndex': 2, 'MatchCollections': [match_collection_dict, ], 'NumMatchCollections': 1}
+match_info_dict = {'NumMatchTypes': 1, 'MatchTypes': [match_dict, ]}
+
+
+class TestMatchCollection(unittest.TestCase):
+    def test_construction(self):
+        the_type = MatchInfo.MatchCollectionType
+        the_dict = match_collection_dict
+        item1 = the_type.from_dict(the_dict)
+
+        with self.subTest(msg='Comparing json deserialization with original'):
+            new_dict = item1.to_dict()
+            self.assertEqual(the_dict, new_dict)
+
+        with self.subTest(msg='Test xml serialization issues'):
+            # let's serialize to xml
+            etree = ElementTree.ElementTree()
+            xml = ElementTree.tostring(item1.to_node(etree, 'The_Type')).decode('utf-8')
+            # let's deserialize from xml
+            node = ElementTree.fromstring(xml)
+            item2 = the_type.from_node(node)
+            self.assertEqual(item1.to_dict(), item2.to_dict())
+
+        with self.subTest(msg='Test validity'):
+            self.assertTrue(item1.is_valid())
+
+
+class TestMatch(unittest.TestCase):
+    def test_construction(self):
+        the_type = MatchInfo.MatchType
+        the_dict = match_dict
+        item1 = the_type.from_dict(the_dict)
+
+        with self.subTest(msg='Comparing json deserialization with original'):
+            new_dict = item1.to_dict()
+            self.assertEqual(the_dict, new_dict)
+
+        with self.subTest(msg='Test xml serialization issues'):
+            # let's serialize to xml
+            etree = ElementTree.ElementTree()
+            xml = ElementTree.tostring(item1.to_node(etree, 'The_Type')).decode('utf-8')
+            # let's deserialize from xml
+            node = ElementTree.fromstring(xml)
+            item2 = the_type.from_node(node)
+            self.assertEqual(item1.to_dict(), item2.to_dict())
+
+        with self.subTest(msg='Test validity'):
+            self.assertTrue(item1.is_valid())
+
+
+class TestMatchInfo(unittest.TestCase):
+    def test_construction(self):
+        the_type = MatchInfo.MatchInfoType
+        the_dict = match_info_dict
+        item1 = the_type.from_dict(the_dict)
+
+        with self.subTest(msg='Comparing json deserialization with original'):
+            new_dict = item1.to_dict()
+            self.assertEqual(the_dict, new_dict)
+
+        with self.subTest(msg='Test xml serialization issues'):
+            # let's serialize to xml
+            etree = ElementTree.ElementTree()
+            xml = ElementTree.tostring(item1.to_node(etree, 'The_Type')).decode('utf-8')
+            # let's deserialize from xml
+            node = ElementTree.fromstring(xml)
+            item2 = the_type.from_node(node)
+            self.assertEqual(item1.to_dict(), item2.to_dict())
+
+        with self.subTest(msg='Test validity'):
+            self.assertTrue(item1.is_valid())
