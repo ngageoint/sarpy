51375edcdf9418ea612c284b9140b5ca83f89bc8
Thomas McCullough <thomas.mccullough@nga.mil>
2020-01-17 15:18:55 -0500

    Completing deprecation proceedure for processing

---
+++ sarpy/processing/normalize_sicd.py
@@ -0,0 +1,175 @@
+"""Functions to transform SICD data to a common state"""
+
+import numpy as np
+from numpy.polynomial import polynomial
+import scipy.signal
+
+__classification__ = "UNCLASSIFIED"
+__author__ = ("Wade Schwartzkopf", "Daniel Haverporth")
+
+
+def is_normalized(sicd_meta, dim=1):
+    """
+
+    Parameters
+    ----------
+    sicd_meta : sarpy.io.complex.sicd_elements.SICD.SICDType
+        the sicd structure
+    dim : int
+        the dimension to test
+
+    Returns
+    -------
+    bool
+        normalization state in the given dimension
+    """
+
+    # TODO: this should be a class method of SICDType
+
+    if dim == 1:  # slow-time
+        sicd_grid_struct = sicd_meta.Grid.Col
+    else:  # fast-time
+        sicd_grid_struct = sicd_meta.Grid.Row
+
+    # Several reasons that we might need to applied normalization
+    needs_deskew = ((sicd_grid_struct.DeltaKCOAPoly is not None) and
+                    np.any(sicd_grid_struct.DeltaKCOAPoly.get_array() != 0))
+    not_uniform_weight = (
+            (sicd_grid_struct.WgtType is not None and sicd_grid_struct.WgtType.WindowName is not None
+             and sicd_grid_struct.WgtType.WindowName != 'UNIFORM') or
+            (sicd_grid_struct.WgtFunct is not None and np.any(np.diff(sicd_grid_struct.WgtFunct))))
+    needs_fft_sgn_flip = (sicd_grid_struct.Sgn == 1)
+    return not (needs_deskew or not_uniform_weight or needs_fft_sgn_flip)
+
+
+def deskewparams(sicd_meta, dim):
+    """
+
+    Parameters
+    ----------
+    sicd_meta: sarpy.io.complex.sicd_elements.SICD.SICDType
+        the sicd structure
+    dim : int
+        the dimension to test
+
+    Returns
+    -------
+    Tuple[numpy.ndarray,numpy.ndarray,numpy.ndarray,float]
+    """
+
+    # TODO: HIGH - this should be class method of SICD class (or something like that)
+
+    # DeltaKCOAPoly
+    DeltaKCOAPoly = np.array([[0, ], ], dtype=np.float64)
+    fft_sgn = -1
+    if dim == 0:
+        try:
+            DeltaKCOAPoly = sicd_meta.Grid.Row.DeltaKCOAPoly.get_array()
+        except (ValueError, AttributeError):
+            pass
+        try:
+            fft_sgn = sicd_meta.Grid.Row.Sgn
+        except (ValueError, AttributeError):
+            pass
+    else:
+        try:
+            DeltaKCOAPoly = sicd_meta.Grid.Col.DeltaKCOAPoly.get_array()
+        except (ValueError, AttributeError):
+            pass
+        try:
+            fft_sgn = sicd_meta.Grid.Col.Sgn
+        except (ValueError, AttributeError):
+            pass
+
+    # Vectors describing range and azimuth distances from SCP (in meters) for rows and columns
+    rg_coords_m = (np.arange(0, sicd_meta.ImageData.NumRows, dtype=np.float32) +
+                   sicd_meta.ImageData.FirstRow - sicd_meta.ImageData.SCPPixel.Row)*sicd_meta.Grid.Row.SS
+    az_coords_m = (np.arange(0, sicd_meta.ImageData.NumCols, dtype=np.float32) +
+                   sicd_meta.ImageData.FirstCol - sicd_meta.ImageData.SCPPixel.Col)*sicd_meta.Grid.Col.SS
+    return DeltaKCOAPoly, rg_coords_m, az_coords_m, fft_sgn
+
+
+def deskewmem(input_data, DeltaKCOAPoly, dim0_coords_m, dim1_coords_m, dim, fft_sgn=-1):
+    """
+    Performs deskew (centering of the spectrum on zero frequency) on a complex dataset.
+
+    Parameters
+    ----------
+    input_data : numpy.ndarray
+        Complex FFT Data
+    DeltaKCOAPoly : numpy.ndarray
+        Polynomial that describes center of frequency support of data.
+    dim0_coords_m : numpy.ndarray
+    dim1_coords_m : numpy.ndarray
+    dim : int
+    fft_sgn : int|float
+
+    Returns
+    -------
+    Tuple[numpy.ndarray, numpy.ndarray]
+        * `output_data` - Deskewed data
+        * `new_DeltaKCOAPoly` - Frequency support shift in the non-deskew dimension caused by the deskew.
+    """
+
+    # TODO: HIGH - this should be class method of SICD class (or something like that)
+
+    # Integrate DeltaKCOA polynomial (in meters) to form new polynomial DeltaKCOAPoly_int
+    DeltaKCOAPoly_int = polynomial.polyint(DeltaKCOAPoly, axis=dim)
+    # New DeltaKCOAPoly in other dimension will be negative of the derivative of
+    # DeltaKCOAPoly_int in other dimension (assuming it was zero before).
+    new_DeltaKCOAPoly = - polynomial.polyder(DeltaKCOAPoly_int, axis=dim-1)
+    # Apply phase adjustment from polynomial
+    dim1_coords_m_2d, dim0_coords_m_2d = np.meshgrid(dim1_coords_m, dim0_coords_m)
+    output_data = np.multiply(input_data, np.exp(1j * fft_sgn * 2 * np.pi *
+                                                 polynomial.polyval2d(
+                                                     dim0_coords_m_2d,
+                                                     dim1_coords_m_2d,
+                                                     DeltaKCOAPoly_int)))
+    return output_data, new_DeltaKCOAPoly
+
+
+def deweightmem(input_data, weight_fun=None, oversample_rate=1, dim=1):
+    """
+    Uniformly weights complex SAR data in given dimension.
+
+    Parameters
+    ----------
+    input_data : numpy.ndarray
+        The complex data
+    weight_fun : callable|numpy.ndarray
+        Can be an array that explicitly provides the (inverse) weighting, or a function of a
+        single numeric argument (number of elements) which produces the (inverse) weighting vector.
+    oversample_rate : int|float
+        Amount of sampling beyond the ImpRespBW in the processing dimension.
+    dim : int
+        Dimension over which to perform deweighting.
+
+    Returns
+    -------
+    numpy.ndarray
+
+    .. Note: This implementation ASSUMES that the data has already been de-skewed and that the frequency support
+        is centered.
+    """
+    # TODO: HIGH - this should be class method of SICD class (or something like that)
+    #   Note that there was a prexisting comment "Test this function"
+
+    # Weighting only valid across ImpRespBW
+    weight_size = round(input_data.shape[dim]/oversample_rate)
+    if weight_fun is None:  # No weighting passed in.  Do nothing.
+        return input_data
+    elif callable(weight_fun):
+        weighting = weight_fun(weight_size)
+    elif np.array(weight_fun).ndim == 1:
+        weighting = scipy.signal.resample(weight_fun, weight_size)
+    # TODO: HIGH - half complete condition
+
+    weight_zp = np.ones((input_data.shape[dim], ), dtype=np.float64)  # Don't scale outside of ImpRespBW
+    weight_zp[np.floor((input_data.shape[dim]-weight_size)/2)+np.arange(weight_size)] = weighting
+
+    # Divide out weighting in spatial frequency domain
+    output_data = np.fft.fftshift(np.fft.fft(input_data, axis=dim), axes=dim)
+    output_data = output_data/weight_zp
+    output_data = np.fft.ifft(np.fft.ifftshift(output_data, axes=dim), axis=dim)
+
+    return output_data
---
+++ sarpy/processing/subaperture.py
@@ -0,0 +1,222 @@
+"""Subaperture processing methods"""
+
+import numpy as np
+
+# Default parameters for a variety of methods
+_DEFAULT_FRAMES = 9
+_DEFAULT_APFRACTION = 0.2
+_DEFAULT_METHOD = 'FULLPIXEL'
+_DEFAULT_DIR = 'RIGHT'
+_DEFAULT_DIM = 1
+_DEFAULT_FILL = 1
+_DEFAULT_OFFSETPCT = None
+_DEFAULT_SELECTEDFRAMES = None
+
+__author__ = ('Clayton Williams', 'Wade Schwartzkopf', 'Tom Braun')
+__classification__ = "UNCLASSIFIED"
+
+# TODO: it seems likely that this should be stream-lined.
+
+
+def mem(ci,
+        frames=_DEFAULT_FRAMES,
+        apfraction=_DEFAULT_APFRACTION,
+        method=_DEFAULT_METHOD,
+        platformdir=_DEFAULT_DIR,
+        dim=_DEFAULT_DIM,
+        fill=_DEFAULT_FILL,
+        offset_pct=_DEFAULT_OFFSETPCT,
+        selected_frames=_DEFAULT_SELECTEDFRAMES):
+    """
+    Calculates the subaperture-processed image on complex data that is held in memory.
+
+    Parameters
+    ----------
+    ci : numpy.ndarray
+        the complex data array
+    frames : int
+        number of frames
+    apfraction : float
+        Fraction of aperture for each subaperture.
+    method : str
+        One of ['NORMAL', 'FULLPIXEL', 'MINIMAL'].
+    platformdir : str
+        Platform direction one of ['RIGHT', 'LEFT'].
+    dim : int
+        Dimension over which to split subaperture.
+    fill : int|float
+        Fill factor.
+    offset_pct : float
+        In the range [0, 1]. Only used for single frame at a certain aperture offset.
+    selected_frames : list|tuple
+        subcollection of [0,...,`frames`-1] determining subset of frames to be used.
+
+    Returns
+    -------
+    list
+        the list of frame data
+
+    .. Note: All desired frames will be held in memory at once.
+    .. Warning: The input array is assumed to have been de-skewed already.
+    """
+
+    return mem_ph(np.fft.fftshift(np.fft.fft(ci, axis=dim), axes=dim),
+                  frames=frames,
+                  apfraction=apfraction,
+                  method=method,
+                  platformdir=platformdir,
+                  dim=dim,
+                  fill=fill,
+                  offset_pct=offset_pct,
+                  selected_frames=selected_frames)
+
+
+def mem_ph(ph,
+           frames=_DEFAULT_FRAMES,
+           apfraction=_DEFAULT_APFRACTION,
+           method=_DEFAULT_METHOD,
+           platformdir=_DEFAULT_DIR,
+           dim=_DEFAULT_DIM,
+           fill=_DEFAULT_FILL,
+           offset_pct=_DEFAULT_OFFSETPCT,
+           selected_frames=_DEFAULT_SELECTEDFRAMES):
+    """
+    Calculates the subaperture-processed image on phase history data that is held in memory.
+
+    Parameters
+    ----------
+    ph: numpy.ndarray
+        the complex data array
+    frames : int
+        number of frames
+    apfraction : float
+        Fraction of aperture for each subaperture.
+    method : str
+        One of ['NORMAL', 'FULLPIXEL', 'MINIMAL'].
+    platformdir : str
+        Platform direction one of ['RIGHT', 'LEFT'].
+    dim : int
+        Dimension over which to split subaperture.
+    fill : int|float
+        Fill factor.
+    offset_pct : float
+        In the range [0, 1]. Only used for single frame at a certain aperture offset.
+    selected_frames : list|tuple
+        subcollection of [0,...,`frames`-1] determining subset of frames to be used.
+
+    Returns
+    -------
+    list
+        the list of frame data
+
+    .. Note: All desired frames will be held in memory at once.
+    .. Warning: Assumes that the data has had an FFTSHIFT applied to it -
+        DC is in the center, not at index 0.
+    """
+
+    # Parse and validate arguments if not already done
+    if not np.iscomplexobj(ph) or ph.ndim > 2:
+        raise(TypeError('Input image must be a single complex image.'))
+
+    # Setup for the subaperture processing
+    if dim == 0:  # simple
+        ph = ph.T  # this is just a view
+
+    if selected_frames is None or frames == 1:
+        selected_frames = list(range(0, frames))
+    elif not isinstance(selected_frames, (list, tuple)):
+        selected_frames = list(selected_frames)
+
+    nxfftOrig = ph.shape[1]
+    left_edge = int(np.round(nxfftOrig * (1 - (1 / max(fill, 1))) / 2))  # Where left edge starts
+    nxfft = nxfftOrig - (2*left_edge)
+    if method.upper() == 'MINIMAL':
+        output_res = np.ceil(nxfft / frames)
+    elif method.upper() == 'FULLPIXEL':
+        output_res = nxfftOrig
+    else:
+        output_res = np.ceil(apfraction*nxfftOrig)
+
+    result = []
+    num_sa = np.ceil(apfraction * nxfft)
+    if frames != 1:
+        step = np.floor((nxfft - num_sa) / (frames - 1))
+    else:
+        step = 0
+    if frames == 1 and offset_pct is not None:  # Use offset_pct, rather than frames
+        if dim == 1 and platformdir.upper() == 'RIGHT':
+            offset_pct = 1 - offset_pct
+        offset = left_edge + np.floor((nxfft - num_sa) * offset_pct)
+    else:
+        offset = left_edge + np.floor((nxfft - (step * (frames - 1) + num_sa)) / 2)
+
+    # Run the subaperture processing.  Done a frame at a time instead of by line -- either works
+    for f in selected_frames:
+        if dim == 1 and platformdir.upper() == 'RIGHT':
+            frame_num = frames - f - 1
+        else:
+            frame_num = f
+        result.append(np.fft.ifft(ph[:, int(np.round(offset + (step * frame_num))):int(np.round(offset + (step * frame_num) + num_sa))],
+                      n=int(output_res)))
+        if dim == 0:
+            result[-1] = result[-1].transpose()
+
+    return result
+
+
+def mem_sicd(ci, sicd_meta,
+             frames=_DEFAULT_FRAMES,
+             apfraction=_DEFAULT_APFRACTION,
+             method=_DEFAULT_METHOD,
+             dim=_DEFAULT_DIM,
+             offset_pct=_DEFAULT_OFFSETPCT,
+             selected_frames=_DEFAULT_SELECTEDFRAMES):
+    """
+    Calculates the subaperture-processed image on complex data that is held in memory, with the
+    fill and platform direction from SICD metadata structure.
+
+    Parameters
+    ----------
+    ci : numpy.array
+    sicd_meta : sarpy.io.complex.sicd_elements.SICD.SICDType
+    frames : int
+        number of frames
+    apfraction : float
+        Fraction of aperture for each subaperture.
+    method : str
+        One of ['NORMAL', 'FULLPIXEL', 'MINIMAL'].
+    dim : int
+        Dimension over which to split subaperture.
+    offset_pct : float
+        In the range [0, 1]. Only used for single frame at a certain aperture offset.
+    selected_frames : list|tuple
+        subcollection of [0,...,`frames`-1] determining subset of frames to be used.
+
+    Returns
+    -------
+    list
+        the list of frame data
+
+    .. Warning: The input array is passed staright through to mem, and so is
+        assumed to have been de-skewed already.
+    """
+
+    if dim == 1:
+        fill = 1/(sicd_meta.Grid.Col.SS * sicd_meta.Grid.Col.ImpRespBW)
+    else:
+        fill = 1/(sicd_meta.Grid.Row.SS * sicd_meta.Grid.Row.ImpRespBW)
+
+    if sicd_meta.SCPCOA.SideOfTrack == 'L':
+        platformdir = 'left'
+    else:
+        platformdir = 'right'
+
+    return mem(ci,
+               frames=frames,
+               apfraction=apfraction,
+               method=method,
+               dim=dim,
+               offset_pct=offset_pct,
+               selected_frames=selected_frames,
+               fill=fill,
+               platformdir=platformdir)
