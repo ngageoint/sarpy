d6d1e10c93dbc7fb62a037afd7280f6364aefdb4
Thomas McCullough <thomas.mccullough@nga.mil>
2020-01-17 14:00:10 -0500

    Moving forward with refactored processing

--- sarpy/io/complex/__init__.py
+++ sarpy/io/complex/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
 __classification__ = "UNCLASSIFIED"
+
+
+def open(file_name):
+    # TODO: flesh this out
+    raise NotImplementedError
+
+
+def convert(input_file, output_file):
+    # TODO: flesh this out
+    raise NotImplementedError
