2ddfc118de5d39fea309ceb59262ef2046f8d077
Jason Casey <j.casey@beamio.net>
2020-01-21 23:44:04 -0500

    working on plot animations within the guide

--- tkinter_gui_builder/panel_templates/pyplot_panel/PyplotPanel.py
+++ tkinter_gui_builder/panel_templates/pyplot_panel/PyplotPanel.py
@@ -4,8 +4,13 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib.collections import LineCollection
 from tkinter_gui_builder.panel_templates.widget_panel.widget_panel import AbstractWidgetPanel
+import time
 
 import numpy as np
+
+
+SCALE_Y_AXIS_PER_FRAME_TRUE = "scale y axis per frame"
+SCALE_Y_AXIS_PER_FRAME_FALSE = "don't scale y axis per frame"
 
 
 class PyplotCanvas(tk.LabelFrame):
@@ -21,19 +26,20 @@ class PyplotCanvas(tk.LabelFrame):
 
 class PyplotControlPanel(AbstractWidgetPanel):
     scale = basic_widgets.Scale         # type: basic_widgets.Scale
+    rescale_y_axis_per_frame = basic_widgets.Combobox         # type: basic_widgets.Combobox
+    animate = basic_widgets.Button          # type: basic_widgets.Button
+    fps_label = basic_widgets.Label         # type: basic_widgets.Label
+    fps_entry = basic_widgets.Entry               # type: basic_widgets.Entry
 
     def __init__(self, parent):
         AbstractWidgetPanel.__init__(self, parent)
+        widget_list = ["scale", "rescale_y_axis_per_frame", "fps_label", "fps_entry", "animate"]
 
-        widget_list = ["scale"]
-
-        self.init_w_horizontal_layout(widget_list)
-
+        self.init_w_basic_widget_list(widget_list, 3, [1, 1, 3])
         self.scale.set(0)
-        self.scale.pack(side='bottom')
-        self.scale.state(["disabled"])
-
-        # master, orient = tk.HORIZONTAL, length = 284, from_ = 0, to = 100)
+        self.rescale_y_axis_per_frame.update_combobox_values([SCALE_Y_AXIS_PER_FRAME_TRUE, SCALE_Y_AXIS_PER_FRAME_FALSE])
+        self.fps_label.set_text("fps")
+        self.fps_entry.set_text("10")
 
 
 class AppVariables():
@@ -48,8 +54,11 @@ class AppVariables():
 
         self.y_margin = 0.05
         self.set_y_margins_per_frame = False
+        self.n_frames = 1
 
         self.segments = None  # type: np.ndarray
+
+        self.animation_related_controls = []
 
 
 class PyplotPanel(AbstractWidgetPanel):
@@ -63,8 +72,27 @@ class PyplotPanel(AbstractWidgetPanel):
         widget_list = ["pyplot_canvas", "control_panel"]
         self.init_w_vertical_layout(widget_list)
 
+        canvas_size_pixels = self.pyplot_canvas.canvas.figure.get_size_inches() * self.pyplot_canvas.canvas.figure.dpi
+
+        self.control_panel.scale.config(length=canvas_size_pixels[0] * 0.75)
+        self.variables.animation_related_controls = [self.control_panel.scale,
+                                                     self.control_panel.rescale_y_axis_per_frame,
+                                                     self.control_panel.animate,
+                                                     self.control_panel.fps_entry,
+                                                     self.control_panel.fps_label]
+
         # set listeners
         self.control_panel.scale.on_left_mouse_motion(self.callback_update_from_slider)
+        self.control_panel.rescale_y_axis_per_frame.on_selection(self.callback_set_y_rescale)
+        self.control_panel.animate.on_left_mouse_click(self.callback_animate)
+
+    def hide_animation_related_controls(self):
+        for widget in self.variables.animation_related_controls:
+            widget.pack_forget()
+
+    def show_animation_related_controls(self):
+        for widget in self.variables.animation_related_controls:
+            widget.pack()
 
     def set_y_margin_percent(self,
                              percent_0_to_100=5          # type: float
@@ -73,25 +101,27 @@ class PyplotPanel(AbstractWidgetPanel):
 
     def set_data(self, plot_data, x_axis=None):
         x = x_axis
-        n_times = 1
+        n_frames = 1
         if len(plot_data.shape) == 1:
+            self.hide_animation_related_controls()
             nx = len(plot_data)
             segments = np.zeros((1, nx, 2))
             segments[0, :, 1] = plot_data
         elif len(plot_data.shape) == 2:
+            self.hide_animation_related_controls()
             nx = len(plot_data[:, 0])
             n_overplots = len(plot_data[0])
             segments = np.zeros((n_overplots, nx, 2))
             for i in range(n_overplots):
                 segments[i, :, 1] = plot_data[:, i]
         elif len(plot_data.shape) == 3:
+            self.show_animation_related_controls()
             nx = np.shape(plot_data)[0]
             n_overplots = np.shape(plot_data)[1]
-            n_times = np.shape(plot_data)[2]
+            n_frames = np.shape(plot_data)[2]
             segments = np.zeros((n_overplots, nx, 2))
             for i in range(n_overplots):
                 segments[i, :, 1] = plot_data[:, i, 0]
-            self.control_panel.scale.state(['!disabled'])
         if x is None:
             x = np.arange(nx)
         segments[:, :, 0] = x
@@ -107,8 +137,9 @@ class PyplotPanel(AbstractWidgetPanel):
         self.variables.x_axis = x
         self.variables.plot_data = plot_data
         self.variables.segments = segments
+        self.variables.n_frames = n_frames
 
-        self.control_panel.scale.config(to=n_times-1)
+        self.control_panel.scale.config(to=n_frames-1)
 
         if len(plot_data.shape) == 3:
             self.update_plot_animation(0)
@@ -139,8 +170,33 @@ class PyplotPanel(AbstractWidgetPanel):
         self.pyplot_canvas.canvas.draw()
 
     def callback_update_from_slider(self, event):
-        if "disabled" in self.control_panel.scale.state():
-            pass
+        animation_index = int(np.round(self.control_panel.scale.get()))
+        self.update_plot_animation(animation_index)
+
+    # define custom callbacks here
+    def callback_set_y_rescale(self, event):
+        selection = self.control_panel.rescale_y_axis_per_frame.get()
+        if selection == SCALE_Y_AXIS_PER_FRAME_TRUE:
+            self.variables.set_y_margins_per_frame = True
         else:
-            animation_index = int(np.round(self.control_panel.scale.get()))
-            self.update_plot_animation(animation_index)
+            self.variables.set_y_margins_per_frame = False
+            y_range = self.variables.plot_data.max() - self.variables.plot_data.min()
+            self.variables.ymin = self.variables.plot_data.min() - y_range * self.variables.y_margin
+            self.variables.ymax = self.variables.plot_data.max() + y_range * self.variables.y_margin
+
+    def animate(self):
+        fps = float(self.control_panel.fps_entry.get())
+        self.control_panel.scale.set(20)
+        self.update_plot_animation(20)
+
+        self.control_panel.scale.set(25)
+        self.update_plot_animation(25)
+
+        self.control_panel.scale.set(30)
+        self.update_plot_animation(30)
+
+        self.control_panel.scale.set(50)
+        self.update_plot_animation(50)
+
+    def callback_animate(self, event):
+        self.animate()
--- tkinter_gui_builder/widgets/widget_wrappers/label.py
+++ tkinter_gui_builder/widgets/widget_wrappers/label.py
@@ -6,3 +6,6 @@ class Label(tk.Label, WidgetEvents):
     def __init__(self, master=None, cnf={}, **kw):
         super(tk.Label, self).__init__(master, 'label', cnf, kw)
         super(WidgetEvents, self).__init__()
+
+    def set_text(self, txt):
+        self.config(text=txt)
