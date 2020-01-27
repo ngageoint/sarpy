fe3601f6c59ef1669b625ee3c639c1fadc0a4fd1
Thomas McCullough <thomas.mccullough@nga.mil>
2020-01-27 10:07:45 -0500

    Feeble nitf header test start

---
+++ tests/io/test_nitf_headers.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+from sarpy.io.nitf_headers import NITFDetails
+
+test_root = 'C:/Users/jr80407/Desktop/sarpy_testing/sicd'
+
+# TODO: refactor this into unit tests
+
+# test_file = os.path.join(test_root, 'sicd_example_RMA_RGZERO_RE32F_IM32F_cropped_multiple_image_segments_v1.2.nitf')
+test_file = os.path.join(test_root, 'sicd_example_RMA_RGZERO_RE16I_IM16I.nitf')
+
+start = time.time()
+details = NITFDetails(test_file)
+# how long does it take?
+print('unpacked nitf details in {}'.format(time.time() - start))
+
+# how does it look?
+# print(details._nitf_header)
+
+# is the output as long as it should be?
+header_string = details._nitf_header.to_string()
+assert len(header_string) == details._nitf_header.HL
+
+# is the output what it should be?
+with open(test_file, 'rb') as fi:
+    file_header = fi.read(details._nitf_header.HL)
+
+# chunk_size = 80
+# start_chunk = 0
+# while start_chunk < len(header_string):
+#     end_chunk = min(start_chunk+chunk_size, len(header_string))
+#     print('real[{}:{}] = {}'.format(
+#         start_chunk, end_chunk, file_header[start_chunk:end_chunk]))
+#     print('prod[{}:{}] = {}'.format(
+#         start_chunk, end_chunk, header_string[start_chunk:end_chunk]))
+#     start_chunk = end_chunk
+
+assert file_header == header_string
