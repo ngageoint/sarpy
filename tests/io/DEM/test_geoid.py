93274e87717446cadff415d55bd3835198b4d5d6
Thomas McCullough <thomas.mccullough@nga.mil>
2020-01-27 13:18:57 -0500

    Refactoring prospective tests for geoid and nitf headers

--- tests/io/DEM/test_geoid.py
+++ tests/io/DEM/test_geoid.py
@@ -1,19 +1,18 @@
 import time
 import os
+import logging
 
 import numpy
 
+from . import unittest
+
 import sarpy.io.DEM.geoid as geoid
 
-# TODO: refactor this into unit test(s)
 
+def generic_geoid_test(instance, test_file, geoid_file):
+    assert isinstance(instance, unittest.TestCase)
 
-# establish file location
-stem = 'C:/Users/jr80407/Desktop/sarpy_testing/geoid'
-
-def geoid_interp():
-    # parse test set
-    test_file = os.path.join(stem, 'GeoidHeights.dat')
+    zcol = 4  # TODO: determine this appropriately
     with open(test_file, 'r') as fi:
         lins = fi.read().splitlines()
     lats = numpy.zeros((len(lins),), dtype=numpy.float64)
@@ -23,32 +22,68 @@ def geoid_interp():
         slin = lin.strip().split()
         lats[i] = float(slin[0])
         lons[i] = float(slin[1])
-        zs[i] = float(slin[4])
-    print('number of test points {}'.format(lats.size))
+        zs[i] = float(slin[zcol])
+    logging.info('number of test points {}'.format(lats.size))
 
-    start = time.time()
-    gh = geoid.GeoidHeight(file_name=os.path.join(stem, 'egm2008-5.pgm'))
-    print('time initializing {}'.format(time.time() - start))
-
+    with instance.subTest(msg="Load geoid file"):
+        start = time.time()
+        gh = geoid.GeoidHeight(file_name=geoid_file)
+        logging.info('time loading geoid file {}'.format(time.time() - start))
 
     recs = 10
-    # test one
-    start = time.time()
-    zs1 = gh.get(lats[:recs], lons[:recs], cubic=False)
-    print('linear - time {}, diff {}'.format(time.time() - start, zs1 - zs[:recs]))
-    # test two
-    start = time.time()
-    zs1 = gh.get(lats[:recs], lons[:recs], cubic=True)
-    print('cubic - time {}, diff {}'.format(time.time() - start, zs1 - zs[:recs]))
+    # small linear test
+    with instance.subTest(msg='Small linear interpolation test'):
+        start = time.time()
+        zs1 = gh.get(lats[:recs], lons[:recs], cubic=False)
+        logging.info('linear - time {}, diff {}'.format(time.time() - start, zs1 - zs[:recs]))
+
+    # small cubic test
+    with instance.subTest(msg='Small cubic interpolation test'):
+        start = time.time()
+        zs1 = gh.get(lats[:recs], lons[:recs], cubic=True)
+        logging.info('cubic - time {}, diff {}'.format(time.time() - start, zs1 - zs[:recs]))
+
+    # full linear test
+    with instance.subTest(msg="Full linear interpolation test"):
+        start = time.time()
+        zs1 = gh.get(lats, lons, cubic=False)
+        diff = numpy.abs(zs1 - zs)
+        max_diff = numpy.max(diff)
+        mean_diff = numpy.mean(diff)
+        logging.info('linear - time {}, max diff - {}, mean diff - {}'.format(time.time() - start, max_diff, mean_diff))
+        instance.assertLessEqual(max_diff, 2, msg="Max difference should be less than 2 meters")
+        instance.assertLessEqual(mean_diff, 0.5, msg="Mean difference should be (much) less than 0.5 meters.")
+
+    # full cubic test
+    with instance.subTest(msg="Full cubic interpolation test"):
+        start = time.time()
+        zs1 = gh.get(lats, lons, cubic=True)
+        diff = numpy.abs(zs1 - zs)
+        max_diff = numpy.max(diff)
+        mean_diff = numpy.mean(diff)
+        logging.info('linear - time {}, max diff - {}, mean diff - {}'.format(time.time() - start, max_diff, mean_diff))
+        instance.assertLessEqual(max_diff, 2, msg="Max difference should be less than 2 meters")
+        instance.assertLessEqual(mean_diff, 0.5, msg="Mean difference should be (much) less than 0.5 meters.")
 
 
-    # test one
-    start = time.time()
-    zs1 = gh.get(lats, lons, cubic=False)
-    diff = numpy.abs(zs1 - zs)
-    print('linear - time {}, max diff - {}, mean diff - {}'.format(time.time() - start, numpy.max(diff), numpy.mean(diff)))
-    # test two
-    start = time.time()
-    zs1 = gh.get(lats, lons, cubic=True)
-    diff = numpy.abs(zs1 - zs)
-    print('cubic - time {}, max diff - {}, mean diff - {}'.format(time.time() - start, numpy.max(diff), numpy.mean(diff)))
+class TestGeoidHeight(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        # todo: fix this up
+        cls.test_root = os.path.expanduser(os.path.join('~', 'Desktop', 'sarpy_testing', 'geoid'))
+
+    def test_geoid_height(self):
+        # establish file location
+        test_root = os.path.expanduser(os.path.join('~', 'Desktop', 'sarpy_testing', 'geoid'))
+        test_file = os.path.join(test_root, 'GeoidHeights.dat')
+
+        tested = 0
+        for fil in ['egm2008-5.pgm', 'egm2008-2_5.pgm', 'egm2008-1.pgm']:
+            geoid_file = os.path.join(self.test_root, fil)
+            if os.path.exists(test_file) and os.path.exists(geoid_file):
+                tested += 1
+                generic_geoid_test(self, test_file, geoid_file)
+            else:
+                logging.info('No file {} or {} found'.format(test_file, geoid_file))
+
+        self.assertTrue(tested > 0, msg="No files for testing found")
--- tests/io/test_nitf_headers.py
+++ tests/io/test_nitf_headers.py
@@ -1,44 +1,69 @@
 import os
 import time
+import logging
 
 from sarpy.io.nitf_headers import NITFDetails
 
-test_root = 'C:/Users/jr80407/Desktop/sarpy_testing/sicd'
+from . import unittest
 
-# TODO: refactor this into unit tests
 
-# test_file = os.path.join(test_root, 'sicd_example_RMA_RGZERO_RE32F_IM32F_cropped_multiple_image_segments_v1.2.nitf')
-test_file = os.path.join(test_root, 'sicd_example_RMA_RGZERO_RE16I_IM16I.nitf')
+def generic_nitf_header_test(instance, test_file):
+    assert isinstance(instance, unittest.TestCase)
 
-start = time.time()
-details = NITFDetails(test_file)
-# how long does it take?
-print('unpacked nitf details in {}'.format(time.time() - start))
+    # can we parse it at all? how long does it take?
+    with instance.subTest(msg="header parsing"):
+        start = time.time()
+        details = NITFDetails(test_file)
+        # how long does it take?
+        logging.info('unpacked nitf details in {}'.format(time.time() - start))
+        # how does it look?
+        logging.info(details._nitf_header)
 
-# how does it look?
-# print(details._nitf_header)
+    # is the output as long as it should be?
+    with instance.subTest(msg="header length match"):
+        header_string = details._nitf_header.to_string()
+        equality = (len(header_string) == details._nitf_header.HL)
+        if not equality:
+            print('len(produced header) = {}, nitf_header.HL = {}'.format(len(header_string),
+                                                                          details._nitf_header.HL))
+        instance.assertTrue(equality)
 
-# is the output as long as it should be?
-header_string = details._nitf_header.to_string()
-equality = (len(header_string) == details._nitf_header.HL)
-if not equality:
-    print('len(produced header) = {}, nitf_header.HL = {}'.format(len(header_string), details._nitf_header.HL))
-assert equality
+    # is the output what it should be?
+    with instance.subTest(msg="header content match"):
+        with open(test_file, 'rb') as fi:
+            file_header = fi.read(details._nitf_header.HL)
 
-# is the output what it should be?
-with open(test_file, 'rb') as fi:
-    file_header = fi.read(details._nitf_header.HL)
+        equality = (file_header == header_string)
+        if not equality:
+            chunk_size = 80
+            start_chunk = 0
+            while start_chunk < len(header_string):
+                end_chunk = min(start_chunk + chunk_size, len(header_string))
+                logging.error('real[{}:{}] = {}'.format(
+                    start_chunk, end_chunk, file_header[start_chunk:end_chunk]))
+                logging.error('prod[{}:{}] = {}'.format(
+                    start_chunk, end_chunk, header_string[start_chunk:end_chunk]))
+                start_chunk = end_chunk
+        instance.assertTrue(equality)
 
-equality = (file_header == header_string)
-if not equality:
-    chunk_size = 80
-    start_chunk = 0
-    while start_chunk < len(header_string):
-        end_chunk = min(start_chunk+chunk_size, len(header_string))
-        print('real[{}:{}] = {}'.format(
-            start_chunk, end_chunk, file_header[start_chunk:end_chunk]))
-        print('prod[{}:{}] = {}'.format(
-            start_chunk, end_chunk, header_string[start_chunk:end_chunk]))
-        start_chunk = end_chunk
 
-assert equality
+class TestNITFHeader(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        # todo: fix this up
+        cls.test_root = os.path.expanduser(os.path.join('~', 'Desktop', 'sarpy_testing', 'sicd'))
+
+    def test_nitf_header(self):
+        tested = 0
+        for fil in [
+                'sicd_example_RMA_RGZERO_RE16I_IM16I.nitf',
+                'sicd_example_RMA_RGZERO_RE32F_IM32F.nitf',
+                'sicd_example_RMA_RGZERO_RE32F_IM32F_cropped_multiple_image_segments_v1.2.nitf']:
+            test_file = os.path.join(self.test_root, fil)
+            if os.path.exists(test_file):
+                tested += 1
+                generic_nitf_header_test(self, test_file)
+            else:
+                logging.info('No file {} found'.format(test_file))
+
+        self.assertTrue(tested > 0, msg="No files for testing found")
