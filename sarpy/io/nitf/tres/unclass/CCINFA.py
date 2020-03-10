9dfac1b34c02e28633865945ecc7b75b4fd671e5
Thomas McCullough <thomas.mccullough@nga.mil>
2020-03-10 12:49:51 -0400

    Added 4 more tre definitions

---
+++ sarpy/io/nitf/tres/unclass/CCINFA.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from ..tre_elements import TREExtension, TREElement
+
+__classification__ = "UNCLASSIFIED"
+__author__ = "Thomas McCullough"
+
+
+class CODE(TREElement):
+    def __init__(self, value):
+        super(CODE, self).__init__()
+        self.add_field('CODE_LEN', 'd', 1, value)
+        self.add_field('CODE', 's', self.CODE_LEN, value)
+        self.add_field('EQTYPE', 's', 1, value)
+        self.add_field('ESURN_LEN', 'd', 2, value)
+        self.add_field('ESURN', 's', self.ESURN_LEN, value)
+        self.add_field('DETAIL_LEN', 'd', 5, value)
+        if self.DETAIL_LEN > 0:
+            self.add_field('DETAIL_CMPR', 's', 1, value)
+            self.add_field('DETAIL', 's', self.DETAIL_LEN, value)
+
+
+class CCINFAType(TREElement):
+    def __init__(self, value):
+        super(CCINFAType, self).__init__()
+        self.add_field('NUMCODE', 'd', 3, value)
+        self.add_loop('CODEs', self.NUMCODE, CODE, value)
+
+
+class CCINFA(TREExtension):
+    _tag_value = 'CCINFA'
+    _data_type = CCINFAType
---
+++ sarpy/io/nitf/tres/unclass/RPFDES.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from ..tre_elements import TREExtension, TREElement
+
+__classification__ = "UNCLASSIFIED"
+__author__ = "Thomas McCullough"
+
+
+class SECREC(TREElement):
+    def __init__(self, value):
+        super(SECREC, self).__init__()
+        self.add_field('LOCID', 'b', 1, value)
+        self.add_field('SECLEN', 'b', 1, value)
+        self.add_field('PHYSIDX', 'b', 1, value)
+
+
+class RPFDESType(TREElement):
+    def __init__(self, value):
+        super(RPFDESType, self).__init__()
+        self.add_field('LOCLEN', 'b', 1, value)
+        # TODO: It's unclear whether this should be interpreted as an 8-bit int, or string?
+        if int(self.LOCLEN) > 0:
+            self.add_field('CLTOFF', 'b', 1, value)
+            self.add_field('SECRECS', 'b', 1, value)
+            self.add_field('RECLEN', 'b', 1, value)
+            self.add_field('AGGLEN', 'b', 1, value)
+            self.add_loop('SECRECList', int(self.SECRECS), SECREC, value)
+        self.add_field('UNKNOWN', 'b', len(value) - self._bytes_length, value)
+
+
+class RPFDES(TREExtension):
+    _tag_value = 'RPFDES'
+    _data_type = RPFDESType
