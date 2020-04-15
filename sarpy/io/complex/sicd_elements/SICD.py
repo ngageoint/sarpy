7e48cf7193f90c284a367f2b6a5fe8d82d32c7c4
tommccullough-tenica <57201143+tommccullough-tenica@users.noreply.github.com>
2020-04-15 09:24:58 -0400

    More SCPCOA properties and RNIIRS population repair (#51)
    * Moving extra SCPCOA properties to the correct place
    
    * Making SICD opening effort more robust by skipping SIDD
    
    * Adding a few more SCPCOA properties
    
    * Repairing problem with RNIIRS population

--- sarpy/io/complex/sicd_elements/SICD.py
+++ sarpy/io/complex/sicd_elements/SICD.py
@@ -181,41 +181,6 @@ class SICDType(Serializable):
                 return attribute
         return 'OTHER'
 
-    @property
-    def ROV(self):
-        """
-        The Ratio of Range to Velocity at Center of Aperture time.
-
-        Returns
-        -------
-        float
-        """
-
-        try:
-            GRP = self.GeoData.SCP.ECF.get_array()
-            ARP = self.SCPCOA.ARPPos.get_array()
-            ARV = self.SCPCOA.ARPVel.get_array()
-            return float(numpy.linalg.norm(GRP-ARP)/numpy.linalg.norm(ARV))
-        except AttributeError:
-            logging.warning('ROV cannot be calculated because necessary attributes are missing')
-            return None
-
-    @property
-    def ThetaDot(self):
-        """
-        Derivative of Theta as a function of time.
-
-        Returns
-        -------
-        float
-        """
-
-        try:
-            return float(numpy.sin(numpy.deg2rad(self.SCPCOA.DopplerConeAng))/self.ROV)
-        except AttributeError:
-            logging.warning('Theta_Dot cannot be calculated because necessary attributes are missing')
-            return None
-
     def _validate_image_segment_id(self):  # type: () -> bool
         if self.ImageFormation is None or self.RadarCollection is None:
             return False
@@ -824,16 +789,17 @@ class SICDType(Serializable):
         None
         """
 
-        try:
-            val = self.CollectionInfo.Parameters['PREDICTED_RNIIRS']  # TODO: verify correct
-            if val is not None:
-                if override:
-                    logging.warning('PREDICTED_RNIIRS already populated, and this value will be overridden.')
-                else:
-                    logging.info('PREDICTED_RNIIRS already populated. Nothing to be done.')
-                    return
-        except Exception:
-            pass
+        if self.CollectionInfo is None:
+            logging.error('CollectionInfo must not be None. Nothing to be done.')
+            return
+
+        if self.CollectionInfo.Parameters is not None and \
+                self.CollectionInfo.Parameters.get('PREDICTED_RNIIRS', None) is not None:
+            if override:
+                logging.warning('PREDICTED_RNIIRS already populated, and this value will be overridden.')
+            else:
+                logging.info('PREDICTED_RNIIRS already populated. Nothing to be done.')
+                return
 
         if noise is None:
             try:
@@ -867,12 +833,16 @@ class SICDType(Serializable):
                 signal = 0.25
 
         try:
-            bw_area = abs(self.Grid.Row.ImpRespBW*self.Grid.Col.ImpRespBW*numpy.cos(numpy.deg2rad(self.SCPCOA.SlopeAng)))
+            bw_area = abs(self.Grid.Row.ImpRespBW*self.Grid.Col.ImpRespBW*
+                          numpy.cos(numpy.deg2rad(self.SCPCOA.SlopeAng)))
         except Exception as e:
             logging.error('Encountered an error estimating bandwidth area. {}'.format(e))
             return
 
         inf_density, rniirs = snr_to_rniirs(bw_area, signal, noise)
-        logging.info('Calculated INFORMATION_DENSITY = {0:0.5G}, PREDICTED_RNIIRS = {1:0.5G}'.format(inf_density, rniirs))
+        logging.info('Calculated INFORMATION_DENSITY = {0:0.5G}, '
+                     'PREDICTED_RNIIRS = {1:0.5G}'.format(inf_density, rniirs))
+        if self.CollectionInfo.Parameters is None:
+            self.CollectionInfo.Parameters = []  # initialize
         self.CollectionInfo.Parameters['INFORMATION_DENSITY'] = '{0:0.2G}'.format(inf_density)
         self.CollectionInfo.Parameters['PREDICTED_RNIIRS'] = '{0:0.1f}'.format(rniirs)
