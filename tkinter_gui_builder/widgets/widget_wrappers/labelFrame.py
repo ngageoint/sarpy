cea8f7439a8fcd611edb4d9cc10c0f5eb0222a4f
tommccullough-tenica <57201143+tommccullough-tenica@users.noreply.github.com>
2020-06-22 09:33:29 -0400

    Separate sarpy and other tools into respective repositories (#71)
    sarpy_apps has split into project/repository https://github.com/ngageoint/sarpy_apps
    tk_builder has split into project/repositiy https://github.com/ngageoint/tk_builder

--- tkinter_gui_builder/widgets/widget_wrappers/labelFrame.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-from tkinter_gui_builder.widgets.widget_utils.widget_events import WidgetEvents
-
-
-class LabelFrame(tkinter.LabelFrame, WidgetEvents):
-    def __init__(self, master=None, cnf=None, **kw):
-        cnf = {} if cnf is None else cnf
-        tkinter.LabelFrame.__init__(self, master=master, cnf=cnf, **kw)
