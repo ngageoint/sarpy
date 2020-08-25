59e24f9ef7a1f647dcebf5f9d00ba9686ed1eb25
tommccullough-tenica <57201143+tommccullough-tenica@users.noreply.github.com>
2020-08-25 16:08:18 -0400

    Added helper methods for aperture tool processing (#113)

--- sarpy/__about__.py
+++ sarpy/__about__.py
@@ -27,7 +27,7 @@ __all__ = ['__version__',
            '__license__', '__copyright__']
 
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 
 __classification__ = "UNCLASSIFIED"  # This should be set appropriately in any high-side version
