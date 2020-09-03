c6637cfa8519655e506d7d1e16c0a9826cd2e3c9
Thomas McCullough <rsl.thomas.mccullough@gmail.com>
2020-09-03 10:39:23 -0400

    Bump in the numpy version dependency for proper datetime handling

--- sarpy/__about__.py
+++ sarpy/__about__.py
@@ -27,7 +27,7 @@ __all__ = ['__version__',
            '__license__', '__copyright__']
 
 
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 
 
 __classification__ = "UNCLASSIFIED"  # This should be set appropriately in any high-side version
