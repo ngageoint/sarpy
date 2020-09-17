1858a02cbfda103fc20c9bae0b004c4212170fe8
tommccullough-tenica <57201143+tommccullough-tenica@users.noreply.github.com>
2020-09-17 16:59:20 -0400

    Very basic example refinement (#126)

--- docs/examples/polygon_example.py
+++ docs/examples/polygon_example.py
@@ -2,57 +2,78 @@ import numpy
 from matplotlib import pyplot
 import time
 
-import sys
-sys.path.append('../..')
-
 from sarpy.geometry.geometry_elements import Polygon
 
 
-def random_points():
-    # create our polygon object
-    segment_count = 12
-    # starts at (0, 0) and goes up to (1, 0) with negative y coordinates by <segment_count> random steps,
-    # then back to (0, 0) with positive y coordinates by <segment_count> random steps.
+def generate_random_polygon(segment_count=12):
+    """
+    Generate the coordinates for a random polygon going from (-1, 0) to (1, 0) and back.
+    It will be contained in square [-1, 1] x [-1, 1].
+
+    Parameters
+    ----------
+    segment_count : int
+
+    Returns
+    -------
+    numpy.ndarray
+    """
+
+    # starts at (-1, 0) and goes up to (1, 0) with negative y coordinates by <segment_count> random steps,
+    # then back to (-1, 0) with positive y coordinates by <segment_count> random steps.
     coords = numpy.zeros((2 * segment_count + 1, 2), dtype=numpy.float64)
     # fill in the randomly generated x coordinates
     x1 = numpy.random.rand(segment_count + 1)
     x1[0] = 0
     l1 = numpy.cumsum(x1)
-    coords[:segment_count+1, 0] = l1 / l1[-1]
+    coords[:segment_count+1, 0] = -1 + 2*l1/l1[-1]
     x2 = numpy.cumsum(numpy.random.rand(segment_count))
-    coords[segment_count + 1:, 0] = 1 - x2 / x2[-1]
+    coords[segment_count + 1:, 0] = 1 - 2*x2/x2[-1]
     # fill in the randomly generated y coordinates
     coords[1:segment_count, 1] = -numpy.random.rand(segment_count - 1)
     coords[segment_count+1:2*segment_count, 1] = numpy.random.rand(segment_count - 1)
+    return coords
 
+
+def basic_check():
+    """
+    Example for checks on a basic polygon.
+
+    Returns
+    -------
+    None
+    """
+
+    # create our polygon object with coordinates bounded by square [0, 1]x[-1, 1]
+    coords = generate_random_polygon()
     poly = Polygon(coordinates=[coords, ])
 
-    # craft some sample points
+    #############################
+    # perform random samples check
     samples = 10000
 
-    pts = numpy.random.rand(samples, 2)
-    pts[:, 0] *= 1.2
-    pts[:, 0] -= 0.1
-    pts[:, 1] *= 2
-    pts[:, 1] -= 1
-
+    pts = 2.2*numpy.random.rand(samples, 2) - 1.1
 
     start = time.time()
     in_poly_condition = poly.contain_coordinates(pts[:, 0], pts[:, 1])
     lapsed = time.time() - start
-    print('lapsed = {}, lapsed/point = {}'.format(lapsed, lapsed/samples))
+    print('basic poly: lapsed = {}, lapsed/point = {}'.format(lapsed, lapsed/samples))
 
-    # now, do grid check
+    ###########################
+    # perform grid check
     grid_samples = 1001
-    x_grid = numpy.linspace(-0.1, 1.1, grid_samples)
+    x_grid = numpy.linspace(-1.1, 1.1, grid_samples)
     y_grid = numpy.linspace(-1.1, 1.1, grid_samples)
 
     start = time.time()
     in_poly_condition2 = poly.grid_contained(x_grid[:-1], y_grid[:-1])
     lapsed = time.time() - start
-    print('lapsed = {}, lapsed/point = {}'.format(lapsed, lapsed/((grid_samples - 1)**2)))
+    print('basic poly: lapsed = {}, lapsed/point = {}'.format(lapsed, lapsed/((grid_samples - 1)**2)))
 
+    #############################
+    # visualize results
     fig, axs = pyplot.subplots(nrows=2, ncols=1, sharex='col', sharey='col')
+    fig.suptitle('Basic polygon example')
     axs[0].scatter(pts[in_poly_condition, 0], pts[in_poly_condition, 1], color='r', marker='.', s=16)
     axs[0].scatter(pts[~in_poly_condition, 0], pts[~in_poly_condition, 1], color='b', marker='.', s=16)
     axs[0].plot(coords[:, 0], coords[:, 1], 'k-')
@@ -60,3 +81,59 @@ def random_points():
     axs[1].pcolormesh(x2d, y2d, in_poly_condition2, cmap='jet')
     axs[1].plot(coords[:, 0], coords[:, 1], 'k-', lw=2, zorder=99)
     pyplot.show()
+
+
+def compound_poly_check():
+    """
+    Example for compound polygon with a hole in it.
+
+    Returns
+    -------
+    None
+    """
+
+    # create our polygon object with coordinates bounded by square [0, 1]x[-1, 1]
+    outer_coords = numpy.array([
+        [-1, 0], [-0.5, -1], [0.5, -1], [1, 0], [0.5, 1], [-0.5, 1], [-1, 0],], dtype='float64')
+    inner_coords = 0.5*generate_random_polygon()
+    poly = Polygon(coordinates=[outer_coords, inner_coords])
+
+    #############################
+    # perform random samples check
+    samples = 10000
+    pts = 2.2*numpy.random.rand(samples, 2) - 1.1
+
+    start = time.time()
+    in_poly_condition = poly.contain_coordinates(pts[:, 0], pts[:, 1])
+    lapsed = time.time() - start
+    print('compound poly: lapsed = {}, lapsed/point = {}'.format(lapsed, lapsed/samples))
+
+    ###########################
+    # perform grid check
+    grid_samples = 1001
+    x_grid = numpy.linspace(-1.1, 1.1, grid_samples)
+    y_grid = numpy.linspace(-1.1, 1.1, grid_samples)
+
+    start = time.time()
+    in_poly_condition2 = poly.grid_contained(x_grid[:-1], y_grid[:-1])
+    lapsed = time.time() - start
+    print('compound poly: lapsed = {}, lapsed/point = {}'.format(lapsed, lapsed/((grid_samples - 1)**2)))
+
+    #############################
+    # visualize results
+    fig, axs = pyplot.subplots(nrows=2, ncols=1, sharex='col', sharey='col')
+    fig.suptitle('Compound polygon example')
+    axs[0].scatter(pts[in_poly_condition, 0], pts[in_poly_condition, 1], color='r', marker='.', s=16)
+    axs[0].scatter(pts[~in_poly_condition, 0], pts[~in_poly_condition, 1], color='b', marker='.', s=16)
+    axs[0].plot(outer_coords[:, 0], outer_coords[:, 1], 'k-')
+    axs[0].plot(inner_coords[:, 0], inner_coords[:, 1], 'k-')
+    y2d, x2d = numpy.meshgrid(y_grid, x_grid, indexing='xy')
+    axs[1].pcolormesh(x2d, y2d, in_poly_condition2, cmap='jet')
+    axs[1].plot(outer_coords[:, 0], outer_coords[:, 1], 'k-')
+    axs[1].plot(inner_coords[:, 0], inner_coords[:, 1], 'k-')
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    basic_check()
+    compound_poly_check()
--- docs/examples/sarpy_example.py
+++ docs/examples/sarpy_example.py
@@ -1,41 +1,92 @@
 """
-Just a quick demo to show the basics of how to use SarPy
+Just a quick demo to show the basics of how to use sarpy is a very basic sense.
+
+If you are just leanring to use python, don't be afraid to use the help utility
+to browse documentation.
+
+from the python prompt - check out a given module, class, or function using it's path
+>>> help('sarpy')
+# or
+>>> help('sarpy.io.complex.sicd.SICDReader')
+
+# or, check out a variable that has been defined somehow
+>>> from sarpy.io.complex.converter import open_complex
+>>> reader = open_complex('<path to file>')
+>>> help(reader)
+
+**NOTE:** the sarpy_apps project provides robust interactive tools.
 """
 
-import matplotlib.pyplot as plt
-import sarpy.io.complex as cf
-import sarpy.visualization.remap as remap
+################
+# Open file from a specified file name
+from sarpy.io.complex.converter import open_complex
 
-# Open file
-fname = 'C:/Temp/my_sicd.nitf'
-ro = cf.open(fname)
+reader = open_complex('<path to file>')
+# this provides an instance of one of the reader classes defined in the modules in sarpy.io.complex
+# a sicd file will necessarily be composed of a single image, but other file formats
+# (sentinel, radarsat, others) often contain multiple images combined into a single
+# package (i.e. multiple polarizations or other aggregate collections)
 
-# Access SICD metadata (even if file read is not in SICD format)
-print(ro.sicd_meta)  # Displays metadata from file in SICD format in human-readable form
-print(ro.sicd_meta.CollectionInfo.CollectorName)  # extracting fields from metadata
+
+################
+# SICD metadata structure
+# Access the single SICD structure or tuple of structures associated with the reader
+# Note that the sicd structure is defined using elements of sarpy.io.complex.sicd_elements
+
+nebulous_contents = reader.sicd_meta
+# this will be an instance of the sicd structure, or a tuple of sicd structures
+# (if multiple images)
+
+# Unified access - this will always be a tuple of sicd structures,
+# with one sicd structure per image
+sicd_tuple = reader.get_sicds_as_tuple()
+
+the_sicd = sicd_tuple[0]  # access the desired sicd structure
+# provide a human readable, if long, contents to terminal
+print(the_sicd)
+# get xml string representation
+xml_string = the_sicd.to_xml_string(tag='SICD')
+# get json friendly dict representation
+dict_representation = the_sicd.to_dict()
+
+# access field values
+print(the_sicd.CollectionInfo.CollectorName)
 
 ###############
-# Read complex pixel data from file
-# cdata = ro[:]  # Reads all complex data from file
+# Read complex pixel data from file - use slice notation
+# data = reader[row_start:row_end:row_step, col_start:col_end:col_step, image_index=0]
 
-# Read every 10th pixel:
-cdata = ro[::10, ::10]
+# in the event of a single image segment
+all_data = reader[:]  # or reader[:, :] - reads all data
+decimated_data = reader[::10, ::10] # reads every 10th pixel
 
-fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
-axs[0].imshow(remap.density(cdata), cmap='gray')  # Display subsampled image
+# in the event of multiple image segments - use another slice index (defaults to 0)
+# the image segment index defaults to 0 for multi-segment images
 
+all_data = reader[:, :, 0]  # reads all data from the image segment at index 0
+# = reader[:, :], reader[:] - yields same result, since image index defaults to 0
+
+
+#################
+# Show some basic plots of the data
+from matplotlib import pyplot
+import sarpy.visualization.remap as remap
+
+fig, axs = pyplot.subplots(nrows=1, ncols=3, figsize=(12, 4))
+axs[0].imshow(remap.density(reader[::10, ::10]), cmap='gray')
 # Reads every other row and column from the first thousand rows and columns:
-cdata = ro[:1000:2, :1000:2]
-axs[1].imshow(remap.density(cdata), cmap='gray')  # Display subsampled image
-
+axs[1].imshow(remap.density(reader[:1000:2, :1000:2]), cmap='gray')  # Display subsampled image
 # Reads every row and column from the first thousand rows and columns:
-cdata = ro[0:1000, 0:1000]
-axs[2].imshow(remap.density(cdata), cmap='gray')  # Display subsampled image
-plt.show()
+axs[2].imshow(remap.density(reader[0:1000, 0:1000]), cmap='gray')  # Display subsampled image
+pyplot.show()
+
 
 ##############
 # Convert a complex dataset (in any format handled by SarPy) to SICD
-cf.convert(fname, 'C:/Temp/new_sicd.nitf')
-# Convert a complex data to SIO
-output_directory = 'C:/Temp'
-cf.convert(fname, output_directory, output_format='sio')
+
+from sarpy.io.complex.converter import conversion_utility
+# to SICD format
+conversion_utility('<complex format file>', '<output_directory>')
+# to SIO format
+conversion_utility('<complex format file>', '<output_directory>', output_format='sio')
+# see help(sarpy.io.complex.converter.conversion_utility)
