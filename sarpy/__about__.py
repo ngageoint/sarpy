0e18658e3a3c8a661bfba118a57a33b67b336b68
Thomas McCullough <rsl.thomas.mccullough@gmail.com>
2020-11-13 15:48:56 -0500

    Fixed bug in PFA validation inspection

--- sarpy/__about__.py
+++ sarpy/__about__.py
@@ -27,7 +27,7 @@ __all__ = ['__version__',
            '__license__', '__copyright__']
 
 
-__version__ = "1.1.27"
+__version__ = "1.1.28"
 
 
 __classification__ = "UNCLASSIFIED"  # This should be set appropriately in any high-side version
