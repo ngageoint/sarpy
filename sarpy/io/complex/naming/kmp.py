b9a7e16daf2db8e817dcb858fb763383665d098c
tommccullough-tenica <57201143+tommccullough-tenica@users.noreply.github.com>
2020-12-01 15:51:44 -0500

    Bug fixes for Cosmo Skymed and fine-tuning KompSat support (#158)

---
+++ sarpy/io/complex/naming/kmp.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from sarpy.io.complex.naming.utils import get_pass_number
+
+__classification__ = "UNCLASSIFIED"
+__author__ = "Thomas McCullough"
+
+_orbits_per_day = 15.0357
+
+
+def get_commercial_id(collector, cdate_str, cdate_mins, product_number):
+    """
+    Gets the commercial id, if appropriate.
+
+    Parameters
+    ----------
+    collector : str
+    cdate : datetime.datetime
+    cdate_str : str
+    cdate_mins : float
+    product_number : int
+
+    Returns
+    -------
+    None|str
+    """
+
+    if not collector.startswith('KMPS'):
+        return None
+
+    crad = 'KS'
+    cvehicle = '0'+collector[4:5]
+    pass_number = get_pass_number(cdate_mins, _orbits_per_day)
+
+    return '{0:s}{1:s}{2:s}{3:s}{4:03d}'.format(cdate_str, crad, cvehicle, pass_number, product_number)
