d414344cd97c4e8e3eba4d6063f500ff2ad10356
Thomas McCullough <thomas.mccullough@gmail.com>
2021-03-24 16:52:33 -0400

    Introduction of Cosmo SkyMed 2nd generation support

---
+++ sarpy/io/complex/naming/csg_naming.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from sarpy.io.complex.naming.utils import get_pass_number
+
+__classification__ = "FOUO"
+__author__ = "Thomas McCullough"
+
+_orbits_per_day = 14.8125  # TODO: is this correct?
+
+
+def get_commercial_id(collector, cdate_str, cdate_mins, product_number):
+    """
+    Gets the commercial id, if appropriate.
+
+    Parameters
+    ----------
+    collector : str
+    cdate_str : str
+    cdate_mins : float
+    product_number : int
+
+    Returns
+    -------
+    None|str
+    """
+
+    if not collector.startswith('SSA'):  # TODO: is this general?
+        return None
+
+    crad = 'CS'
+    cvehicle = collector[3:5]
+    pass_number = get_pass_number(cdate_mins, _orbits_per_day)
+
+    return '{0:s}{1:s}{2:s}{3:s}{4:03d}'.format(cdate_str, crad, cvehicle, pass_number, product_number)
