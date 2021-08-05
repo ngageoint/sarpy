c556e2b3c7523813071342e0c5ab841a4dd593c9
Thomas McCullough <thomas.mccullough@gmail.com>
2021-08-05 17:38:35 -0400

    Preliminarily fleshing out the Image Info

--- sarpy/annotation/afrl_elements/DetailImageInfo.py
+++ sarpy/annotation/afrl_elements/DetailImageInfo.py
@@ -7,8 +7,22 @@ __authors__ = ("Thomas McCullough", "Thomas Rackers")
 
 from typing import Optional
 
+import numpy
+
 # noinspection PyProtectedMember
-from sarpy.io.complex.sicd_elements.base import _StringDescriptor, Serializable
+from sarpy.io.complex.sicd_elements.base import _StringDescriptor, Serializable, \
+    _SerializableDescriptor, _IntegerDescriptor, _StringEnumDescriptor, \
+    _DateTimeDescriptor, _FloatDescriptor
+from sarpy.io.complex.sicd_elements.blocks import RowColType
+from .base import DEFAULT_STRICT
+from .blocks import RangeCrossRangeType, RowColDoubleType
+
+# TODO: Review what's marked required/oprional - I'm sure it makes little sense
+#  Questionable field definitions:
+#   - there is a PixelSpacing and then slant/ground plane elements for pixel spacing.
+#   - 3dbWidth is a poorly formed name, so I am omitting for now
+#   - ZuluOffset seemingly assumes that the only possible offsets are integer valued - this is wrong
+#   - DataCalibrated should obviously be xs:boolean - this is kludged badly for no reason
 
 
 class ClassificationMarkingsType(Serializable):
@@ -53,18 +67,228 @@ class ClassificationMarkingsType(Serializable):
         super(ClassificationMarkingsType, self).__init__(**kwargs)
 
 
-# TODO: to be completed
-
-class DetailImageInfoType(Serializable):
-    _fields = ()  # fill this in
-    _required = ()  # fill this in
-
+class StringRangeCrossRangeType(Serializable):
+    """
+    A range and cross range attribute container
+    """
+    _fields = ('Range', 'CrossRange')
+    _required = _fields
     # descriptors
+    Range = _StringDescriptor(
+        'Range', _required, strict=True, docstring='The Range attribute.')  # type: str
+    CrossRange = _StringDescriptor(
+        'CrossRange', _required, strict=True, docstring='The Cross Range attribute.')  # type: str
 
-    def __init__(self, **kwargs):
+    def __init__(self, Range=None, CrossRange=None, **kwargs):
         """
         Parameters
         ----------
+        Range : str
+        CrossRange : str
+        kwargs : dict
+        """
+
+        if '_xml_ns' in kwargs:
+            self._xml_ns = kwargs['_xml_ns']
+        if '_xml_ns_key' in kwargs:
+            self._xml_ns_key = kwargs['_xml_ns_key']
+        self.Range, self.CrossRange = Range, CrossRange
+        super(StringRangeCrossRangeType, self).__init__(**kwargs)
+
+
+class ImageCornerType(Serializable):
+    _fields = (
+        'LeftFront', 'RightFront', 'RightRear', 'LeftRear')
+    _required = _fields
+    # descriptors
+    LeftFront = _SerializableDescriptor(
+        'LeftFront', RowColDoubleType, _required, strict=DEFAULT_STRICT,
+        docstring='')  # type: RowColDoubleType
+    RightFront = _SerializableDescriptor(
+        'RightFront', RowColDoubleType, _required, strict=DEFAULT_STRICT,
+        docstring='')  # type: RowColDoubleType
+    RightRear = _SerializableDescriptor(
+        'RightRear', RowColDoubleType, _required, strict=DEFAULT_STRICT,
+        docstring='')  # type: RowColDoubleType
+    LeftRear = _SerializableDescriptor(
+        'LeftRear', RowColDoubleType, _required, strict=DEFAULT_STRICT,
+        docstring='')  # type: RowColDoubleType
+
+    def __init__(self, LeftFront=None, RightFront=None,
+                 RightRear=None, LeftRear=None, **kwargs):
+        """
+        Parameters
+        ----------
+        LeftFront : RowColDoubleType|numpy.ndarray|list|tuple
+        RightFront : RowColDoubleType|numpy.ndarray|list|tuple
+        RightRear : RowColDoubleType|numpy.ndarray|list|tuple
+        LeftRear : RowColDoubleType|numpy.ndarray|list|tuple
+        kwargs : dict
+        """
+
+        if '_xml_ns' in kwargs:
+            self._xml_ns = kwargs['_xml_ns']
+        if '_xml_ns_key' in kwargs:
+            self._xml_ns_key = kwargs['_xml_ns_key']
+        self.LeftFront = LeftFront
+        self.RightFront = RightFront
+        self.RightRear = RightRear
+        self.LeftRear = LeftRear
+        super(ImageCornerType, self).__init__(**kwargs)
+
+
+class PixelSpacingType(Serializable):
+    _fields = ('PixelSpacing', )
+    _required = _fields
+    # descriptors
+    PixelSpacing = _SerializableDescriptor(
+        'PixelSpacing', RangeCrossRangeType, _required, strict=DEFAULT_STRICT,
+        docstring='The center-to-center pixel spacing in meters.')  # type: RangeCrossRangeType
+
+    def __init__(self, PixelSpacing=None, **kwargs):
+        """
+        Parameters
+        ----------
+        PixelSpacing : RangeCrossRangeType
+        kwargs
+        """
+
+        if '_xml_ns' in kwargs:
+            self._xml_ns = kwargs['_xml_ns']
+        if '_xml_ns_key' in kwargs:
+            self._xml_ns_key = kwargs['_xml_ns_key']
+        self.PixelSpacing = PixelSpacing
+        super(PixelSpacingType, self).__init__(**kwargs)
+
+
+class DetailImageInfoType(Serializable):
+
+    _fields = (
+        'DataFilename', 'ClassificationMarkings', 'Filetype', 'DataCheckSum',
+        'DataSize', 'DataPlane', 'DataDomain', 'DataType', 'BitsPerSample',
+        'DataByteOrder', 'NumPixels', 'ImageCollectionDate', 'ZuluOffset',
+        'SensorReferencePoint', 'SensorCalibrationFactor', 'DataCalibrated',
+        'Resolution', 'PixelSpacing', 'WeightingType', 'OverSamplingFactor',
+        'ImageQualityDescription', 'ImageHeading',
+        'ImageCorners', 'SlantPlane', 'GroundPlane')
+    _required = (
+        'DataFilename', 'ClassificationMarkings', 'DataPlane',
+        'NumPixels', 'PixelSpacing', 'ImageCorners')
+    # descriptors
+    DataFilename = _StringDescriptor(
+        'DataFilename', _required, docstring='The image file name')  # type: str
+    ClassificationMarkings = _SerializableDescriptor(
+        'ClassificationMarkings', ClassificationMarkingsType, _required,
+        docstring='The classification information')  # type: ClassificationMarkingsType
+    Filetype = _StringDescriptor(
+        'Filetype', _required,
+        docstring='The image file type')  # type: Optional[str]
+    DataCheckSum = _StringDescriptor(
+        'DataCheckSum', _required,
+        docstring='The image file checksum')  # type: Optional[str]
+    DataSize = _IntegerDescriptor(
+        'DataSize', _required,
+        docstring='The image size in bytes')  # type: Optional[int]
+    DataPlane = _StringEnumDescriptor(
+        'DataPlane', {'Slant', 'Ground'}, _required, default_value='Slant',
+        docstring='The image plane.')  # type: str
+    DataDomain = _StringDescriptor(
+        'DataDomain', _required,
+        docstring='The image data domain')  # type: Optional[str]
+    DataType = _StringDescriptor(
+        'DataType', _required,
+        docstring='The image data type')  # type: Optional[str]
+    BitsPerSample = _IntegerDescriptor(
+        'BitsPerSample', _required,
+        docstring='The number of bits per sample')  # type: Optional[int]
+    DataByteOrder = _StringEnumDescriptor(
+        'DataByteOrder', {'Big-Endian', 'Little-Endian'}, _required,
+        docstring='The image data byte order.')  # type: Optional[str]
+    NumPixels = _SerializableDescriptor(
+        'NumPixels', RowColType, _required,
+        docstring='The number of image pixels')  # type: RowColType
+    ImageCollectionDate = _DateTimeDescriptor(
+        'ImageCollectionDate', _required,
+        docstring='The date/time of the image collection in UTC')  # type: Optional[numpy.datetime64]
+    ZuluOffset = _IntegerDescriptor(
+        'ZuluOffset', _required,
+        docstring='The local time offset from UTC')  # type: Optional[int]  # TODO: this isn't always integer
+    SensorReferencePoint = _StringEnumDescriptor(
+        'DataPlane', {'Left', 'Right', 'Top', 'Bottom'}, _required,
+        docstring='Description of the sensor location relative to the scene.')  # type: Optional[str]
+    SensorCalibrationFactor = _FloatDescriptor(
+        'SensorCalibrationFactor', _required,
+        docstring='Multiplicative factor used to scale raw image data to the return '
+                  'of a calibrated reference reflector or active source')  # type: Optional[float]
+    DataCalibrated = _StringDescriptor(
+        'DataCalibrated', _required,
+        docstring='Has the data been calibrated?')  # type: Optional[str]  # TODO: this obviously should be a xs:boolean
+    Resolution = _SerializableDescriptor(
+        'Resolution', RangeCrossRangeType, _required,
+        docstring='Resolution (intrinsic) of the sensor system/mode in meters.')  # type: RangeCrossRangeType
+    PixelSpacing = _SerializableDescriptor(
+        'PixelSpacing', RangeCrossRangeType, _required,
+        docstring='Pixel spacing of the image in meters.')  # type: RangeCrossRangeType
+    WeightingType = _SerializableDescriptor(
+        'WeightingType', StringRangeCrossRangeType, _required,
+        docstring='Weighting function applied to the image during formation.')  # type: StringRangeCrossRangeType
+    OverSamplingFactor = _SerializableDescriptor(
+        'OverSamplingFactor', RangeCrossRangeType, _required,
+        docstring='The factor by which the pixel space is oversampled.')  # type: RangeCrossRangeType
+    ImageQualityDescription = _StringDescriptor(
+        'ImageQualityDescription', _required,
+        docstring='General description of image quality')  # type: Optional[str]
+    ImageHeading =  _FloatDescriptor(
+        'ImageHeading', _required,
+        docstring='Image heading relative to True North, in degrees')  # type: Optional[float]
+    ImageCorners = _SerializableDescriptor(
+        'ImageCorners', ImageCornerType, _required,
+        docstring='The image corners')  # type: ImageCornerType
+    SlantPlane = _SerializableDescriptor(
+        'SlantPlane', PixelSpacingType, _required,
+        docstring='The slant plane pixel spacing')  # type: Optional[PixelSpacingType]
+    GroundPlane = _SerializableDescriptor(
+        'GroundPlane', PixelSpacingType, _required,
+        docstring='The ground plane pixel spacing')  # type: Optional[PixelSpacingType]
+
+    def __init__(self, DataFilename=None, ClassificationMarkings=None,
+                 FileType=None, DataCheckSum=None, DataSize=None,
+                 DataPlane='Slant', DataDomain=None, DataType=None,
+                 BitsPerSample=None, DataByteOrder=None, NumPixels=None,
+                 ImageCollectionDate=None, ZuluOffset=None,
+                 SensorReferencePoint=None, SensorCalibrationFactor=None,
+                 DataCalibrated=None, Resolution=None, PixelSpacing=None,
+                 WeightingType=None, OverSamplingFactor=None,
+                 ImageQualityDescription=None, ImageHeading=None, ImageCorners=None,
+                 SlantPlane=None, GroundPlane=None, **kwargs):
+        """
+        Parameters
+        ----------
+        DataFilename : str
+        ClassificationMarkings : ClassificationMarkingsType
+        FileType : str
+        DataCheckSum : None|str
+        DataSize : int
+        DataPlane : str
+        DataDomain : None|str
+        DataType : None|str
+        BitsPerSample : None|int
+        DataByteOrder : None|str
+        NumPixels : RowColType|numpy.ndarray|list|tuple
+        ImageCollectionDate : numpy.datetime64|datetime|date|str
+        ZuluOffset : None|int
+        SensorReferencePoint : None|str
+        SensorCalibrationFactor : None|float
+        DataCalibrated : None|str
+        Resolution : RangeCrossRangeType|numpy.ndarray|list|tuple
+        PixelSpacing : RangeCrossRangeType|numpy.ndarray|list|tuple
+        WeightingType : StringRangeCrossRangeType
+        OverSamplingFactor : RangeCrossRangeType
+        ImageQualityDescription : None|str
+        ImageHeading : None|float
+        ImageCorners : ImageCornerType
+        SlantPlane : None|PixelSpacingType
+        GroundPlane : None|PixelSpacingType
         kwargs
             Other keyword arguments
         """
@@ -73,5 +297,32 @@ class DetailImageInfoType(Serializable):
             self._xml_ns = kwargs['_xml_ns']
         if '_xml_ns_key' in kwargs:
             self._xml_ns_key = kwargs['_xml_ns_key']
+        self.DataFilename = DataFilename
+        if ClassificationMarkings is None:
+            self.ClassificationMarkings = ClassificationMarkingsType()
+        else:
+            self.ClassificationMarkings = ClassificationMarkings
+        self.Filetype = FileType
+        self.DataCheckSum = DataCheckSum
+        self.DataSize = DataSize
+        self.DataPlane = DataPlane
+        self.DataDomain = DataDomain
+        self.DataType = DataType
+        self.BitsPerSample = BitsPerSample
+        self.DataByteOrder = DataByteOrder
+        self.NumPixels = NumPixels
+        self.ImageCollectionDate = ImageCollectionDate
+        self.ZuluOffset = ZuluOffset
+        self.SensorReferencePoint = SensorReferencePoint
+        self.SensorCalibrationFactor = SensorCalibrationFactor
+        self.DataCalibrated = DataCalibrated
+        self.Resolution = Resolution
+        self.PixelSpacing = PixelSpacing
+        self.WeightingType = WeightingType
+        self.OverSamplingFactor = OverSamplingFactor
+        self.ImageQualityDescription = ImageQualityDescription
+        self.ImageHeading = ImageHeading
+        self.ImageCorners = ImageCorners
+        self.SlantPlane = SlantPlane
+        self.GroundPlane = GroundPlane
         super(DetailImageInfoType, self).__init__(**kwargs)
-
