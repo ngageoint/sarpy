151cdbc68d6d4988c198592fc9f7a5ffda579bff
Thomas McCullough <thomas.mccullough@gmail.com>
2021-09-30 21:05:49 -0400

    Relocating some helper functions for more general purposes

--- sarpy/io/complex/utils.py
+++ sarpy/io/complex/utils.py
@@ -7,11 +7,13 @@ __author__ = "Thomas McCullough"
 
 
 import logging
-from typing import Iterator
+from typing import Iterator, Tuple, List
 
 import numpy
 from numpy.polynomial import polynomial
+from scipy.stats import scoreatpercentile
 
+from sarpy.io.general.base import BaseReader
 from sarpy.io.complex.sicd_elements.blocks import Poly2DType
 
 logger = logging.getLogger(__name__)
@@ -270,3 +272,210 @@ def get_physical_coordinates(the_sicd, row_value, col_value):
 
     return get_im_physical_coords(row_value, the_sicd.Grid, the_sicd.ImageData, 'row'), \
            get_im_physical_coords(col_value, the_sicd.Grid, the_sicd.ImageData, 'col')
+
+
+
+###################
+# helper functions
+
+def get_fetch_block_size(start_element, stop_element, block_size_in_bytes, bands=1):
+    """
+    Gets the appropriate block size, given fetch parameters and constraints.
+
+    Note: this is a helper function, and no checking of argument validity will
+    be performed.
+
+    Parameters
+    ----------
+    start_element : int
+    stop_element : int
+    block_size_in_bytes : int
+    bands : int
+
+    Returns
+    -------
+    int
+    """
+
+    if stop_element == start_element:
+        return None
+    if block_size_in_bytes is None:
+        return None
+
+    full_size = float(abs(stop_element - start_element))
+    return max(1, int(numpy.ceil(block_size_in_bytes / float(bands*8*full_size))))
+
+
+def extract_blocks(the_range, index_block_size):
+    # type: (Tuple[int, int, int], int) -> (List[Tuple[int, int, int]], List[Tuple[int, int]])
+    """
+    Convert the single range definition into a series of range definitions in
+    keeping with fetching of the appropriate block sizes.
+
+    Note: this is a helper function, and no checking of argument validity will
+    be performed.
+
+    Parameters
+    ----------
+    the_range : Tuple[int, int, int]
+        The input (off processing axis) range.
+    index_block_size : None|int|float
+        The size of blocks (number of indices).
+
+    Returns
+    -------
+    List[Tuple[int, int, int]], List[Tuple[int, int]]
+        The sequence of range definitions `(start index, stop index, step)`
+        relative to the overall image, and the sequence of start/stop indices
+        for positioning of the given range relative to the original range.
+    """
+
+    entries = numpy.arange(the_range[0], the_range[1], the_range[2], dtype=numpy.int64)
+    if index_block_size is None:
+        return [the_range, ], [(0, entries.size), ]
+
+    # how many blocks?
+    block_count = int(numpy.ceil(entries.size/float(index_block_size)))
+    if index_block_size == 1:
+        return [the_range, ], [(0, entries.size), ]
+
+    # workspace for what the blocks are
+    out1 = []
+    out2 = []
+    start_ind = 0
+    for i in range(block_count):
+        end_ind = start_ind+index_block_size
+        if end_ind < entries.size:
+            block1 = (int(entries[start_ind]), int(entries[end_ind]), the_range[2])
+            block2 = (start_ind, end_ind)
+        else:
+            block1 = (int(entries[start_ind]), the_range[1], the_range[2])
+            block2 = (start_ind, entries.size)
+        out1.append(block1)
+        out2.append(block2)
+        start_ind = end_ind
+    return out1, out2
+
+
+def get_data_mean_magnitude(bounds, reader, index, block_size_in_bytes):
+    """
+    Gets the mean magnitude in the region defined by bounds.
+
+    Note: this is a helper function, and no checking of argument validity will
+    be performed.
+
+    Parameters
+    ----------
+    bounds : numpy.ndarray|tuple|list
+        Of the form `(row_start, row_end, col_start, col_end)`.
+    reader : BaseReader
+        The data reader.
+    index : int
+        The reader index to use.
+    block_size_in_bytes : int|float
+        The block size in bytes.
+
+    Returns
+    -------
+    float
+    """
+
+    # Extract the mean of the data magnitude - for global remap usage
+    logger.info(
+        'Calculating mean over the block ({}:{}, {}:{}), this may be time consuming'.format(*bounds))
+    mean_block_size = get_fetch_block_size(bounds[0], bounds[1], block_size_in_bytes)
+    mean_column_blocks, _ = extract_blocks((bounds[2], bounds[3], 1), mean_block_size)
+    mean_total = 0.0
+    mean_count = 0
+    for this_column_range in mean_column_blocks:
+        data = numpy.abs(reader[
+                         bounds[0]:bounds[1],
+                         this_column_range[0]:this_column_range[1],
+                         index])
+        mask = (data > 0) & numpy.isfinite(data)
+        mean_total += numpy.sum(data[mask])
+        mean_count += numpy.sum(mask)
+    return float(mean_total / mean_count)
+
+
+def stats_calculation(data, percentile=None):
+    """
+    Calculate the statistics for input into the nrl remap.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        The amplitude array, assumed real valued an finite.
+    percentile : None|float|int
+        Which percentile to calculate
+
+    Returns
+    -------
+    tuple
+        Of the form `(`minimum, maximum)` or `(minimum, maximum, `percentile` percentile)
+    """
+
+    if percentile is None:
+        return numpy.min(data), numpy.max(data)
+    else:
+        return numpy.min(data), numpy.max(data), scoreatpercentile(data, percentile)
+
+
+def get_data_extrema(bounds, reader, index, block_size_in_bytes, percentile=None):
+    """
+    Gets the minimum and maximum magnitude in the region defined by bounds,
+    optionally, get as **estimate** for the `percentage` percentile point.
+
+    Note: this is a helper function, and no checking of argument validity will
+    be performed.
+
+    Parameters
+    ----------
+    bounds : numpy.ndarray|tuple|list
+        Of the form `(row_start, row_end, col_start, col_end)`.
+    reader : BaseReader
+        The data reader.
+    index : int
+        The reader index to use.
+    block_size_in_bytes : int|float
+        The block size in bytes.
+    percentile : None|int|float
+        The optional percentile to estimate.
+
+    Returns
+    -------
+    Tuple
+        The minimum (finite) observed, maximum (finite) observed, and optionally,
+        the desired (of finite) percentile.
+
+        The minimum, maximum, and percentile values will be `None` exactly when the reader
+        contains no finite data.
+    """
+
+    min_value = None
+    max_value = None
+    percent = None
+
+    # Extract the mean of the data magnitude - for global remap usage
+    logger.info(
+        'Calculating extrema over the block ({}:{}, {}:{}), this may be time consuming'.format(*bounds))
+    mean_block_size = get_fetch_block_size(bounds[0], bounds[1], block_size_in_bytes)
+    mean_column_blocks, _ = extract_blocks((bounds[2], bounds[3], 1), mean_block_size)
+    for this_column_range in mean_column_blocks:
+        data = numpy.abs(reader[
+                         bounds[0]:bounds[1],
+                         this_column_range[0]:this_column_range[1],
+                         index])
+        mask = numpy.isfinite(data)
+        if numpy.any(mask):
+            temp_values = stats_calculation(data[mask], percentile=percentile)
+
+            min_value = temp_values[0] if min_value is None else min(min_value, temp_values[0])
+            max_value = temp_values[1] if max_value is None else max(max_value, temp_values[1])
+            if percentile is not None:
+                percent = temp_values[2] if percent is None else temp_values[2]
+
+    if percentile is None:
+        return min_value, max_value
+    else:
+        return min_value, max_value, percent
