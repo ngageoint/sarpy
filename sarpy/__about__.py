3d32606478c677f36c9e02d8aec4ee736af2cb03
Thomas McCullough <thomas.mccullough@gmail.com>
2021-10-21 20:30:38 -0400

    version bump

--- sarpy/__about__.py
+++ sarpy/__about__.py
@@ -27,7 +27,7 @@ __all__ = ['__version__',
            '__license__', '__copyright__']
 
 
-__version__ = "1.2.31"
+__version__ = "1.2.32"
 
 
 __classification__ = "UNCLASSIFIED"  # This should be set appropriately in any high-side version
