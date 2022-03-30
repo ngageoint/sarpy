d0f7529e9e497a1deec827dfff44bd91517f4225
Thomas McCullough <thomas.mccullough@gmail.com>
2022-03-30 15:03:37 -0400

    Separating correlation from data reading

--- sarpy/processing/registration/regi.py
+++ sarpy/processing/registration/regi.py
@@ -1,7 +1,11 @@
 """
 Basic image registration, generally best suited most suited for coherent image
-collection. This is mostly based on an approach developed at Sandia which is generally referred to
-by the name "regi".
+collection. This is mostly based on an approach developed at Sandia and generally
+referred to by the name "regi".
+
+The relevant matlab code appears to be authored by
+Terry M. Calloway, Sandia National Laboratories, and modified by
+Wade Schwartzkopf.
 """
 
 __classification__ = 'UNCLASSIFIED'
@@ -40,7 +44,60 @@ logger = logging.getLogger(__name__)
 #   - Note that there is NO ROTATION here
 
 
-def _single_step(
+def _max_correlation_step(reference_array, moving_array):
+    """
+    Find the best match location of the moving array inside the reference array.
+
+    Parameters
+    ----------
+    reference_array : numpy.ndarray
+    moving_array : numpy.ndarray
+
+    Returns
+    -------
+    best_location : None|numpy.ndarray
+        Will return `None` if there is no information, i.e. the reference patch
+        or moving patch is all 0. Otherwise, this will be a numpy array
+        `[row, column]` of the location of highest correlation, determined via
+        :func:`numpy.argmax`.
+    """
+
+    if reference_array.ndim != 2 or moving_array != 2:
+        raise ValueError('Input arrays must be 2-dimensional')
+    if reference_array.shape[0] < moving_array.shape[0] or reference_array.shape[1] < moving_array.shape[1]:
+        raise ValueError(
+            'It is required that the moving array (shape {}) is strictly contained\n\t'
+            'inside  the reference array (shape {})'.format(moving_array.shape, reference_array.shape))
+
+    # NB: sqrt suggested by matlab, presumably to dampen the importance of bright returns?
+    reference_array = numpy.sqrt(numpy.abs(reference_array))
+    if numpy.all(reference_array == 0):
+        return None
+
+    moving_array = numpy.sqrt(numpy.abs(moving_array))
+    if numpy.all(moving_array == 0):
+        return None
+
+    kernel = numpy.ones(reference_array.shape, dtype='float32')
+
+    # now, find the best match
+    match_values = correlate2d(reference_array, moving_array, mode='valid')
+    norm_values = correlate2d(kernel, moving_array*moving_array, mode='valid')
+    mask = (norm_values > 0)
+
+    # reduce to dot product of the unit vectors, and we pick the best match
+    match_values[mask] /= numpy.sqrt(norm_values[mask])
+
+    if (moving_array.shape[0] % 2) == 0 or (moving_array.shape[1] % 2) == 0:
+        shift = numpy.zeros((2, ), dtype='float64')
+    else:
+        shift = numpy.zeros((2, ), dtype='int64')
+    shift[0] = 0.5*(moving_array.shape[0] - 1)
+    shift[1] = 0.5*(moving_array.shape[1] - 1)
+    return shift + numpy.unravel_index(numpy.argmax(match_values), match_values.shape)
+
+
+def _single_step_reader(
         reference_reader, reference_index, moving_reader, moving_index, reference_location, moving_location,
         match_box_size=(25, 25), moving_deviation=(15, 15), decimation=(1, 1)):
     """
@@ -69,7 +126,7 @@ def _single_step(
     ref_size = reference_reader.get_data_size_as_tuple()[reference_index]
     moving_size = moving_reader.get_data_size_as_tuple()[moving_index]
 
-    # TODO: this check should be moved up a level
+    # TODO: this check should be moved up a level?
     if match_box_size[0] > 0.3*ref_size[0] or match_box_size[1] > 0.3*ref_size[1]:
         raise ValueError(
             'The size of the match box ({}) is too close\n\t'
@@ -97,16 +154,13 @@ def _single_step(
             'using reference box of size {} and decimation {}\n\t'
             'at reference location {}'.format(ref_size, match_box_size, decimation, reference_location))
     ref_box_end = (
-        ref_box_start[0]+ match_box_size[0]*decimation[0]+1,
-        ref_box_start[1]+ match_box_size[1]*decimation[1]+1)
-    ref_array = numpy.sqrt(numpy.abs(
-        reference_reader[ref_box_start[0]:ref_box_end[0]:decimation[0],
-        ref_box_start[1]:ref_box_end[1]:decimation[1], reference_index]))
-    if numpy.all(ref_array == 0):
-        return None
+        ref_box_start[0] + match_box_size[0]*decimation[0] + 1,
+        ref_box_start[1] + match_box_size[1]*decimation[1] + 1)
 
-    # sqrt suggested by matlab, presumably to dampen the importance of bright returns
-    ref_array /= numpy.sum(ref_array*ref_array)
+    reference_array = reference_reader[
+                      ref_box_start[0]:ref_box_end[0]:decimation[0],
+                      ref_box_start[1]:ref_box_end[1]:decimation[1],
+                      reference_index]
 
     # vet the suggested moving location
     mov_loc_temp = [moving_location[0], moving_location[1]]
@@ -115,40 +169,23 @@ def _single_step(
             mov_loc_temp[i] = moving_half[i]*decimation[i]
         elif mov_loc_temp[i] > moving_size[i] - moving_half[i]*decimation[i] + 1:
             mov_loc_temp[i] = moving_size[i] - moving_half[i]*decimation[i] + 1
-
-    # fetch the moving patch over which we will optimize
+    # fetch the moving patch area over which we will optimize
     moving_box_start = (
         mov_loc_temp[0] - moving_half[0]*decimation[0],
         mov_loc_temp[1] - moving_half[1]*decimation[1])
-
     moving_box_end = (
         mov_loc_temp[0] + moving_half[0]*decimation[0] + 1,
         mov_loc_temp[1] + moving_half[1]*decimation[1] + 1)
 
-    moving_array = numpy.sqrt(numpy.abs(
-        moving_reader[moving_box_start[0]:moving_box_end[0]:decimation[0],
-        moving_box_start[1]:moving_box_end[1]:decimation[1], moving_index]))
-    if numpy.all(moving_array == 0):
+    moving_array = moving_reader[
+                   moving_box_start[0]:moving_box_end[0]:decimation[0],
+                   moving_box_start[1]:moving_box_end[1]:decimation[1],
+                   moving_index]
+    best_temp_location = _max_correlation_step(reference_array, moving_array)
+
+    if best_temp_location is None:
         return None
 
-    kernel = numpy.ones(ref_array.shape, dtype='float32')
-
-    # now, find the best match
-    match_values = correlate2d(ref_array, moving_array, mode='valid')
-    norm_values = correlate2d(kernel, moving_array*moving_array, mode='valid')
-    mask = (norm_values > 0)
-
-    match_values[mask] /= norm_values[mask]  # this is now the dot product of the unit vectors, let's pick the best match.
-
-    if match_values.shape != moving_deviation:
-        raise ValueError(
-            'Got unanticipated match shape {}\n\texpected {}'.format(match_values.shape, moving_deviation))
-
-    best_temp_location = numpy.unravel_index(numpy.argmax(match_values), match_values.shape)
-    best_location = (
-        (best_temp_location[0] + match_box_half[0])*decimation[0] + mov_loc_temp[0],
-        (best_temp_location[1] + match_box_half[1])*decimation[1] + mov_loc_temp[1])
-
-    return best_location
-
-
+    return (
+        (best_temp_location[0])*decimation[0] + mov_loc_temp[0],
+        (best_temp_location[1])*decimation[1] + mov_loc_temp[1])
