1088c901a49e883fdfdd41fcb4f2e79b46d24a23
Daniel Pressler <Daniel.Pressler@govsco.com>
2024-03-04 09:15:22 -0700

    update changelog; add smoketest

--- tests/io/general/test_tre.py
+++ tests/io/general/test_tre.py
@@ -7,3 +7,8 @@ class TestTreRegistry(unittest.TestCase):
     def test_find_tre(self):
         the_tre = find_tre('ACFTA')
         self.assertEqual(the_tre, ACFTA)
+
+
+def test_matesa(tests_path):
+    example = find_tre('MATESA').from_bytes((tests_path / 'data/example_matesa_tre.bin').read_bytes(), 0)
+    assert example.DATA.GROUPs[-1].MATEs[-1].MATE_ID == 'EO1H1680372005097110PZ.MET'
