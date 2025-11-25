0ef6cb659e5678a7f47bb0d8a168a04e81123c59
petersontex <16402569+petersontex@users.noreply.github.com>
2025-11-25 11:00:13 -0500

    Corrected version number
    removed release candidate marker

--- sarpy/__about__.py
+++ sarpy/__about__.py
@@ -28,7 +28,7 @@ __all__ = ['__version__',
 
 from sarpy.__details__ import __classification__, _post_identifier
 
-__version__ = "1.3.62-rc"
+__version__ = "1.3.62"
 
 __author__ = "National Geospatial-Intelligence Agency"
 __url__ = "https://github.com/ngageoint/sarpy"
