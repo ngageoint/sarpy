f52abc640ca37ed16d2b860535766457e596c8b4
Peterson <529965@bah.com>
2025-12-16 17:34:32 -0500

    Documented parse_datetime.

--- sarpy/io/xml/base.py
+++ sarpy/io/xml/base.py
@@ -566,6 +566,36 @@ def parse_complex(value, name, instance):
 
 
 def parse_datetime(value, name, instance, units='us'):
+    """
+    The parse_datetime function is a helper function specifically for parsing 
+    datetime values within XML elements in sarpy.io.xml.base. 
+    The function is not intended for public use. This function is recursive.
+    
+    Parameters
+    ----------
+    value : ElementTree.Element|None|datetime|dict
+        The ElementTree.Element entity that you want to get the value from.
+        None returns None.
+        datetime returns a datetime.
+        A string value will return the datetime value for the string if it can be 
+        converted to a datetime.
+    name : str 
+        Name of the field to return the value of. This is only used in the 
+        raised error message
+    instance :
+        The class of the variable. This is only used in the raised error message.
+
+    Returns
+    -------
+    None | bool
+        Returns None if value passed is None. Returns the float value
+        of a node when passed an ElementTree.Element
+
+    Raises
+    -------
+    TypeError
+        When passed a value with a type other than the expected input types.
+    """
     if value is None:
         return None
     if isinstance(value, numpy.datetime64):
